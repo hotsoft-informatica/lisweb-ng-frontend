da6593dcfae72a662d7adf47b3f8371e6f51d7e8
RogerDelNobre <rogerdelnobre@gmail.com>
2021-07-06 15:59:04 -0300

    Cria componente template laboratory domain crud

---
+++ src/app/views/laboratory-domain-crud/laboratory-domain-crud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-laboratory-domain-crud',
+  templateUrl: './laboratory-domain-crud.component.html',
+  styleUrls: ['./laboratory-domain-crud.component.css']
+})
+export class LaboratoryDomainCrudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
