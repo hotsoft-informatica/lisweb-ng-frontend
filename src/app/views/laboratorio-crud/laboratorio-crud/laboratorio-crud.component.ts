e846253f31dfc7417f2f5411981da0d9b300eb14
RogerDelNobre <rogerdelnobre@gmail.com>
2021-07-20 08:36:15 -0300

    Cria componente view laboratorio crud

---
+++ src/app/views/laboratorio-crud/laboratorio-crud/laboratorio-crud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-laboratorio-crud',
+  templateUrl: './laboratorio-crud.component.html',
+  styleUrls: ['./laboratorio-crud.component.css']
+})
+export class LaboratorioCrudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
