43f712cc214513a1573375beb572cd516ddce98c
RogerDelNobre <rogerdelnobre@gmail.com>
2021-07-22 15:33:53 -0300

    Ajusta exibicao componente read laboratorio

--- src/app/components/laboratorio/laboratorio-read/laboratorio-read.component.ts
+++ src/app/components/laboratorio/laboratorio-read/laboratorio-read.component.ts
@@ -9,35 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LaboratorioReadComponent implements OnInit {
   laboratorios: Laboratorio[] = [];
-  displayedColumns = [
-    'id',
-    'nome',
-    'access_token',
-    'created_at',
-    'updated_at',
-    'version_counter',
-    'serie',
-    'next_numero_atendimento',
-    'next_numero_orcamento',
-    'status',
-    'next_numero_baixa',
-    'producao_estoque',
-    'producao_financeiro',
-    'sincronizar',
-    'token_nfe',
-    'url_nfe',
-    'laboratory_domain_id',
-    'version_id',
-    'deleted',
-    'empresa_id',
-    'original_id',
-    'synchronized',
-    'domain_id',
-    'criado_em',
-    'changed_by_lab_id',
-    'sync_deadline',
-    'matrix',
-  ];
+  displayedColumns = ['id', 'nome', 'serie', 'criado_em', 'action'];
 
   constructor(private laboratorioService: LaboratorioService) { }
 
