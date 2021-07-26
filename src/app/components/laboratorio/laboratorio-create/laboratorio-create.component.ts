cb567670689a2e55a91cc9c8d376fdd122c46ab8
RogerDelNobre <rogerdelnobre@gmail.com>
2021-07-26 17:20:52 -0300

    Implementa relacionamento laboratorio

--- src/app/components/laboratorio/laboratorio-create/laboratorio-create.component.ts
+++ src/app/components/laboratorio/laboratorio-create/laboratorio-create.component.ts
@@ -1,3 +1,5 @@
+import { LaboratoryDomain } from './../../model/laboratory-domain.model';
+import { LaboratoryDomainService } from '../../laboratory-domain/laboratory-domain.service';
 import { Laboratorio } from '../../model/laboratorio.model';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
@@ -10,16 +12,25 @@ import { STRING_TYPE } from '@angular/compiler';
 })
 export class LaboratorioCreateComponent implements OnInit {
   laboratorio: Laboratorio;
+  laboratoryDomains: LaboratoryDomain[] = [];
 
   constructor(
     private router: Router,
-    private laboratorioService: LaboratorioService
+    private laboratorioService: LaboratorioService,
+    private laboratoryDomainService: LaboratoryDomainService
   ) {
     this.laboratorio = new Laboratorio({});
-    this.laboratorio.laboratory_domain_id = 1;
+    // this.laboratorio.laboratory_domain_id = 1;
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.laboratoryDomainService
+      .read()
+      .subscribe((laboratoryDomains) => {
+        this.laboratoryDomains = laboratoryDomains;
+        console.table(this.laboratoryDomains);
+      });
+  }
 
   createLaboratorio(): void {
     this.laboratorioService.create(this.laboratorio).subscribe(() => {
