1c1797fe7ffb1d76f4f456d48419014d84cda0c4
RogerDelNobre <rogerdelnobre@gmail.com>
2021-10-26 15:35:11 -0300

    Ajustes consulta amostra service

--- src/app/components/service/consulta-amostra.service.ts
+++ src/app/components/service/consulta-amostra.service.ts
@@ -1,4 +1,4 @@
-import { ExameAmostra } from './../model/exame-amostra.model';
+import { ConsultaAmostra } from '../model/consulta-amostra.model';
 import { Query } from './../model/query.model';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -7,7 +7,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 @Injectable({
   providedIn: 'root',
 })
-export class ExameAmostraService {
+export class ConsultaAmostraService {
   baseUrl = 'http://127.0.0.1:3010/exame-amostras';
 
   query: Query[] = [];
@@ -22,36 +22,36 @@ export class ExameAmostraService {
     });
   }
 
-  create(exameAmostra: ExameAmostra): Observable<ExameAmostra> {
-    return this.http.post<ExameAmostra>(this.baseUrl, exameAmostra);
+  create(exameAmostra: ConsultaAmostra): Observable<ConsultaAmostra> {
+    return this.http.post<ConsultaAmostra>(this.baseUrl, ConsultaAmostra);
   }
 
-  read(): Observable<ExameAmostra[]> {
-    return this.http.get<ExameAmostra[]>(this.baseUrl);
+  read(): Observable<ConsultaAmostra[]> {
+    return this.http.get<ConsultaAmostra[]>(this.baseUrl);
   }
 
-  readById(id: number): Observable<ExameAmostra> {
+  readById(id: number): Observable<ConsultaAmostra> {
     const url = `${this.baseUrl}/${id}`;
-    return this.http.get<ExameAmostra>(url);
+    return this.http.get<ConsultaAmostra>(url);
   }
 
-  update(exameAmostra: ExameAmostra): Observable<ExameAmostra> {
-    const url = `${this.baseUrl}/${exameAmostra.id}`;
-    return this.http.put<ExameAmostra>(url, exameAmostra);
+  update(consultaAmostra: ConsultaAmostra): Observable<ConsultaAmostra> {
+    const url = `${this.baseUrl}/${consultaAmostra.id}`;
+    return this.http.put<ConsultaAmostra>(url, consultaAmostra);
   }
 
-  delete(id: number): Observable<ExameAmostra> {
+  delete(id: number): Observable<ConsultaAmostra> {
     const url = `${this.baseUrl}/${id}`;
-    return this.http.delete<ExameAmostra>(url);
+    return this.http.delete<ConsultaAmostra>(url);
   }
 
-  findExameAmostras(
+  findConsultaAmostras(
     active: string = '',
     sortOrder: string = 'asc',
     pageNumber: number = 1,
     pageSize: number = 3,
     query: Query[] | null
-  ): Observable<ExameAmostra[]> {
+  ): Observable<ConsultaAmostra[]> {
     let params = new HttpParams()
       .set('active', active)
       .set('sortOrder', sortOrder)
@@ -64,12 +64,12 @@ export class ExameAmostraService {
       }
     });
 
-    return this.http.get<ExameAmostra[]>(this.baseUrl, {
+    return this.http.get<ConsultaAmostra[]>(this.baseUrl, {
       params,
     });
   }
 
-  countExameAmostras(): Observable<number> {
+  countConsultaAmostras(): Observable<number> {
     return this.http.get<number>(this.baseUrl, {
       params: new HttpParams().set('totalCount', 'true'),
     });
