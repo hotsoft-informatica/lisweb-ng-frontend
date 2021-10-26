8789064107cb5745a7e4595fe7d483eebf2dd9ab
RogerDelNobre <rogerdelnobre@gmail.com>
2021-10-26 17:06:06 -0300

    WIP Desenvolvendo componente e template consulta_amostra

--- src/app/components/consulta-amostra/consulta-amostra-show/consulta-amostra-show.component.ts
+++ src/app/components/consulta-amostra/consulta-amostra-show/consulta-amostra-show.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { ConsultaAmostraService } from './../../service/consulta-amostra.service';
+import { ConsultaAmostra } from '../../model/consulta-amostra.model';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { Query } from '../../model/query.model';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  startWith,
+  tap,
+  delay,
+  filter,
+} from 'rxjs/operators';
+import { merge, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-consulta-amostra-show',
   templateUrl: './consulta-amostra-show.component.html',
-  styleUrls: ['./consulta-amostra-show.component.css']
+  styleUrls: ['./consulta-amostra-show.component.css'],
 })
 export class ConsultaAmostraShowComponent implements OnInit {
+  totalCount!: number;
+  dataSource!: ConsultaAmostra;
+  displayedColumns = [
+    'id',
+    'original_id',
+    'laboratorio_id',
+    'exame_id',
+    'amostra_id',
+    'num_multi_amostra',
+    'created_at',
+    'updated_at',
+    'agrupamento_amostra_id',
+    'fracionamento_amostra_id',
+    'version_id',
+    'laboratory_domain_id',
+    'action'
+  ];
 
-  constructor() { }
+  // @ViewChild(MatPaginator) paginator: MatPaginator | any;
 
+  // @ViewChild(MatSort) sort: MatSort | any;
+
+  query: Query[] = [];
+
+  constructor(private consultaAmostraService: ConsultaAmostraService) { }
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 
+  search(key: string, value: string): void {
+    let query = new Query({ key, value });
+    this.query = this.query.filter((q) => q.key !== key);
+    this.query.push(query);
+    // this.paginator.pageIndex = 0;
+    // this.loadLaboratoriosPage();
+  }
 }
