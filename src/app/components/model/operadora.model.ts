75a053d9064115fb77f28ce3161391a6ca1d02b4
RogerDelNobre <rogerdelnobre@gmail.com>
2022-01-03 16:53:04 -0300

    Cria model de operadora

---
+++ src/app/components/model/operadora.model.ts
@@ -0,0 +1,17 @@
+export class Operadora {
+  public id?: number;
+  public laboratorio_id?: number;
+  public original_id?: number;
+  public created_at?: Date = new Date();
+  public updated_at?: Date = new Date();
+  public version_id?: number;
+  public laboratory_domain_id?: number;
+  public deleted?: boolean;
+  public empresa_id?: number;
+  public criado_em?: Date = new Date();
+  public changed_by_lab_id?: number;
+
+  constructor(values: Operadora) {
+    Object.assign(this, values);
+  }
+}
