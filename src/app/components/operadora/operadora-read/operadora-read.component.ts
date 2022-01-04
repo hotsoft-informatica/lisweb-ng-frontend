25959d512651a95d459825950a8006cd8a515250
RogerDelNobre <rogerdelnobre@gmail.com>
2022-01-04 11:00:48 -0300

    Cria componente operadora read

---
+++ src/app/components/operadora/operadora-read/operadora-read.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-operadora-read',
+  templateUrl: './operadora-read.component.html',
+  styleUrls: ['./operadora-read.component.css']
+})
+export class OperadoraReadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
