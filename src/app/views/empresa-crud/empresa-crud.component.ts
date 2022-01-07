70689f48ce33d5772411d53904b66edad0ba4c61
RogerDelNobre <rogerdelnobre@gmail.com>
2022-01-07 17:23:19 -0300

    Implementa view empresa crud

--- src/app/views/empresa-crud/empresa-crud.component.ts
+++ src/app/views/empresa-crud/empresa-crud.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-empresa-crud',
   templateUrl: './empresa-crud.component.html',
-  styleUrls: ['./empresa-crud.component.css']
+  styleUrls: ['./empresa-crud.component.css'],
 })
 export class EmpresaCrudComponent implements OnInit {
+  constructor(private router: Router) { }
 
-  constructor() { }
+  ngOnInit(): void { }
 
-  ngOnInit(): void {
+  navigateToRequisicaoCreate(): void {
+    this.router.navigate(['/empresas/create']);
   }
-
 }
