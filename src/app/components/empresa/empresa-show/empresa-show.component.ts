8ceee68da57a977bec861a49d47e6d613dcaba9b
RogerDelNobre <rogerdelnobre@gmail.com>
2022-01-18 17:27:25 -0300

    Implementa componente show empresa

--- src/app/components/empresa/empresa-show/empresa-show.component.ts
+++ src/app/components/empresa/empresa-show/empresa-show.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Empresa } from './../../model/empresa.model';
+import { EmpresaService } from '../../service/empresa.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-empresa-show',
   templateUrl: './empresa-show.component.html',
-  styleUrls: ['./empresa-show.component.css']
+  styleUrls: ['./empresa-show.component.css'],
 })
 export class EmpresaShowComponent implements OnInit {
+  empresa!: Empresa;
+  id: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private empresaService: EmpresaService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.load(this.id);
   }
 
+  ngOnInit(): void { }
+
+  load(id: number): void {
+    this.empresaService
+      .readById(id as unknown as number)
+      .subscribe((empresa) => {
+        this.empresa = empresa;
+      });
+  }
+
+  cancel(): void {
+    this.router.navigate(['/empresas']);
+  }
 }
