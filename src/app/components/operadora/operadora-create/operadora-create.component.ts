8cf5ffcaab3ea620bfab624d05b2722841b56b37
RogerDelNobre <rogerdelnobre@gmail.com>
2022-01-20 17:29:18 -0300

    Implementa componente operadora create

--- src/app/components/operadora/operadora-create/operadora-create.component.ts
+++ src/app/components/operadora/operadora-create/operadora-create.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Operadora } from './../../model/operadora.model';
+import { OperadoraService } from '../../service/operadora.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-operadora-create',
   templateUrl: './operadora-create.component.html',
-  styleUrls: ['./operadora-create.component.css']
+  styleUrls: ['./operadora-create.component.css'],
 })
 export class OperadoraCreateComponent implements OnInit {
+  operadora!: Operadora;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private operadoraService: OperadoraService
+  ) {
+    this.operadora = new Operadora({});
   }
 
+  ngOnInit(): void { }
+
+  createOperadora(): void {
+    this.operadoraService.create(this.operadora).subscribe(() => {
+      this.operadoraService.showMessage('Operadora criada com sucesso!');
+      this.router.navigate(['/operadoras']).then(() => {
+        window.location.reload();
+      });
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['/operadoras']);
+  }
 }
