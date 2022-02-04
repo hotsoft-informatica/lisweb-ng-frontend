29a251e5549e39aa8e50db711a97831a30385a9a
RogerDelNobre <rogerdelnobre@gmail.com>
2022-02-04 15:37:56 -0300

    Refatora componente update operadora

--- src/app/components/operadora/operadora-update/operadora-update.component.ts
+++ src/app/components/operadora/operadora-update/operadora-update.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { Empresa } from './../../model/empresa.model';
+import { EmpresaService } from '../../service/empresa.service';
+import { Operadora } from './../../model/operadora.model';
+import { OperadoraService } from '../../service/operadora.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-operadora-update',
   templateUrl: './operadora-update.component.html',
-  styleUrls: ['./operadora-update.component.css']
+  styleUrls: ['./operadora-update.component.css'],
 })
 export class OperadoraUpdateComponent implements OnInit {
+  operadora: Operadora;
+  empresas: Empresa[] = [];
+  empresa!: Empresa;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private empresaService: EmpresaService,
+    private operadoraService: OperadoraService
+  ) {
+    this.operadora = new Operadora({});
   }
 
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.operadoraService
+      .readById(id as unknown as number)
+      .subscribe((operadora) => {
+        this.operadora = operadora;
+        this.empresaService.readById(this.operadora.empresa_id as number).subscribe((empresa) => {
+          this.empresa ||= empresa;
+          console.log("Log da operadora update");
+          console.table(this.empresa);
+          this.operadora.empresa = empresa;
+        });
+      });
+  }
+
+  updateOperadora(): void {
+    this.operadora.empresa_id = this.empresa.id;
+    this.operadora.empresa ||= this.empresa;
+    this.empresaService.update(this.empresa).subscribe(() => {
+      this.operadoraService.showMessage('Empresa atualizada com sucesso!');
+      this.operadoraService.update(this.operadora).subscribe(() => {
+        this.operadoraService.showMessage('Operadora atualizada com sucesso!');
+        this.router.navigate(['/operadoras']).then(() => {
+          window.location.reload();
+        });
+      });
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['/operadoras']);
+  }
 }
