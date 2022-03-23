3805decb81cdb59f7de75d1e31734ad817b6462e
Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>
2022-03-23 09:44:23 -0300

    Alterando componente de login para comportar autenticaçao
    Signed-off-by: Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>

--- src/app/components/login/lg-senha/lg-senha.component.ts
+++ src/app/components/login/lg-senha/lg-senha.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LgSenhaComponent implements OnInit {
   hide = true;
+  serie = 0;
+  senha = '';
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  salveSenha(): void {
+    localStorage.setItem('login', this.senha);
+    console.log(this.senha);
+    console.log(localStorage.getItem('login'));
+  }
+
+  
 }
--- src/app/components/login/lg-usuario/lg-usuario.component.ts
+++ src/app/components/login/lg-usuario/lg-usuario.component.ts
@@ -1,4 +1,9 @@
+import { combineLatest } from 'rxjs';
+import { Usuario } from './../../model/usuario.model';
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../service/usuario.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap,map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-lg-usuario',
@@ -7,8 +12,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LgUsuarioComponent implements OnInit {
   hide = true;
-  constructor() { }
+  serie = 2001;
+  login = '';
+  storage: Storage = window.localStorage;
+  existeUsuario = false;
+  existeEmail = false;
+
+  constructor(private usuarioService: UsuarioService,
+              private router: Router,) { }
 
   ngOnInit(): void {}
-  
+
+  salveLogin(): void {
+    localStorage.setItem('login', this.login);
+    let getUsuario = this.usuarioService.readByUsuario(this.login)
+
+    localStorage.setItem('login', this.login);
+    let getEmail = this.usuarioService.readByEmail(this.login)
+
+    combineLatest([getUsuario, getEmail])
+    .subscribe(result => {
+      this.existeUsuario = (result[0].id as number > 0);
+      this.existeEmail = (result[1].id as number > 0);
+      console.log(this.existeUsuario);
+      console.log(this.existeEmail);
+      if(this.existeUsuario || this.existeEmail){
+        this.router.navigate(['/senha']);
+      }
+      else{
+        console.log('erro, usuario nao econtroado');
+      }
+    })
+
+  }
 }
+
