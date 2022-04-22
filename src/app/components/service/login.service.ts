4efb7589314824925cdc5562fb3cafc1ea02a734
Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>
2022-04-22 09:06:34 -0300

    criando service de login
    Signed-off-by: Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>

---
+++ src/app/components/service/login.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { EMPTY, Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { combineLatest } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+  baseUrl = 'http://127.0.0.1:3010/login';
+
+
+  constructor(private snackbar: MatSnackBar, private http: HttpClient) { }
+
+  showMessage(msg: string): void {
+    this.snackbar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+    });
+  }
+
+  autenticar(login:string, senha:string): Observable<any> {
+    const url = `${this.baseUrl}/${login}`;
+    const autenticacao = this.http.post<any>(url, senha);
+    return combineLatest([autenticacao]);
+  }
+
+}
