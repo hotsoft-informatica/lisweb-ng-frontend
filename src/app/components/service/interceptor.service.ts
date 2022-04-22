7363d95c1d286229f2adb147a72e39140bf4e58a
Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>
2022-04-22 18:02:04 -0300

    criando variavel para token, validando se esta preenchida para autorizacao
    Signed-off-by: Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>

--- src/app/components/service/interceptor.service.ts
+++ src/app/components/service/interceptor.service.ts
@@ -7,8 +7,6 @@ import {
 from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { LgUsuarioComponent } from '../login/lg-usuario/lg-usuario.component';
-import { LgSenhaComponent } from '../login/lg-senha/lg-senha.component';
 
 @Injectable()
 
@@ -16,6 +14,7 @@ export class Interceptor implements HttpInterceptor {
   storage: Storage = window.localStorage;
   user: String = '';
   password: String = '';
+  token: String = '';
   serie = 2001;
 
  intercept( request: HttpRequest<any>, next: HttpHandler ):
@@ -25,21 +24,26 @@ export class Interceptor implements HttpInterceptor {
     let headers = request.headers;
     const password_str = this.storage.getItem('senha') ? this.storage.getItem('senha') : '';
     const user_str = this.storage.getItem('login') ? this.storage.getItem('login') : '';
+    const token_str = this.storage.getItem('token') ? this.storage.getItem('token') : '';
     this.password = new String(password_str);
     this.user = new String(user_str);
+    this.token = new String(token_str);
 
-    if ((this.user.length > 1) && ( this.password.length > 1)){
-      headers = headers.append('user', user_str as string)
-                       .append('password', password_str as string)
-                       .append('serie', this.serie.toString())
+    if(this.token.length > 1){
+      headers = headers.append('token', token_str as string)
+      console.log('Autorizado por token')
     }
-
+    else {
+      if ((this.user.length > 1) && ( this.password.length > 1)){
+        headers = headers.append('user', user_str as string)
+                        .append('password', password_str as string)
+                        .append('serie', this.serie.toString())
+      }
+    }
     const requestAuth = request.clone({
       headers: headers
     });
-
     return next.handle(requestAuth);
-  }
-
-
+ }
 }
+
