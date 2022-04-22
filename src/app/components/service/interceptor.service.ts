c082f3cb6632cff337afe88c7d089b1e68a7288f
Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>
2022-04-22 09:05:24 -0300

    ajustando intecptor
    Signed-off-by: Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>

--- src/app/components/service/interceptor.service.ts
+++ src/app/components/service/interceptor.service.ts
@@ -7,23 +7,39 @@ import {
 from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { LgUsuarioComponent } from '../login/lg-usuario/lg-usuario.component';
+import { LgSenhaComponent } from '../login/lg-senha/lg-senha.component';
 
 @Injectable()
 
 export class Interceptor implements HttpInterceptor {
+  storage: Storage = window.localStorage;
+  user: String = '';
+  password: String = '';
+  serie = 2001;
 
  intercept( request: HttpRequest<any>, next: HttpHandler ):
  Observable<HttpEvent<any>> {
 
-  console.log(request);
+    //TODO: puxar da variavel
+    let headers = request.headers;
+    const password_str = this.storage.getItem('senha') ? this.storage.getItem('senha') : '';
+    const user_str = this.storage.getItem('login') ? this.storage.getItem('login') : '';
+    this.password = new String(password_str);
+    this.user = new String(user_str);
 
-  const requestAuth = request.clone({
-    headers: request.headers.append('user', 'arthurpas')
-                            .append('password', 'isa140198')
-                            .append('serie', '2001')
-  });
+    if ((this.user.length > 1) && ( this.password.length > 1)){
+      headers = headers.append('user', user_str as string)
+                       .append('password', password_str as string)
+                       .append('serie', this.serie.toString())
+    }
 
-  return next.handle(requestAuth);
- }
+    const requestAuth = request.clone({
+      headers: headers
+    });
+
+    return next.handle(requestAuth);
+  }
+
 
 }
