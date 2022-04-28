31124d5df42596c09a89ef9a55ea1b985e8ac902
RogerDelNobre <rogerdelnobre@gmail.com>
2022-04-28 16:56:23 -0300

    Cria Componente create e template de responsavel tecnico

--- src/app/components/responsavel-tecnico/responsavel-tecnico-create/responsavel-tecnico-create.component.ts
+++ src/app/components/responsavel-tecnico/responsavel-tecnico-create/responsavel-tecnico-create.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ResponsavelTecnico } from '../../model/responsavel-tecnico.model';
+import { ResponsavelTecnicoService } from '../../service/responsavel-tecnico.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-responsavel-tecnico-create',
   templateUrl: './responsavel-tecnico-create.component.html',
   styleUrls: ['./responsavel-tecnico-create.component.css']
 })
 export class ResponsavelTecnicoCreateComponent implements OnInit {
+  responsavelTecnico: ResponsavelTecnico;
 
-  constructor() { }
+  constructor(private router: Router, private responsavelTecnicoService: ResponsavelTecnicoService) {
+    this.responsavelTecnico = new ResponsavelTecnico({});
+  }
 
   ngOnInit(): void {
+  }
+
+  createResponsavelTecnico(): void {
+    this.responsavelTecnicoService.create(this.responsavelTecnico).subscribe(() => {
+      this.responsavelTecnicoService.showMessage('Responsável Técnico criado com sucesso!');
+      this.router.navigate(['/responsavel_tecnicos']).then(() => {
+        window.location.reload();
+      });
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['/responsavel_tecnicos']);
   }
 
 }
