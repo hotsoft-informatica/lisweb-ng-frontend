bcdf9d90d550755c7163816bc186c257828e2ddf
Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>
2022-05-27 17:20:34 -0300

    passando comandos para funcao sair
    Signed-off-by: Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>

--- src/app/components/service/logout.service.ts
+++ src/app/components/service/logout.service.ts
@@ -1,9 +1,11 @@
+import { Router } from '@angular/router';
 import { UsuarioToken } from './../model/usuario-token.model';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { EMPTY, Observable } from 'rxjs';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { combineLatest } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +14,8 @@ export class LogoutService {
   baseUrl = 'http://127.0.0.1:3010/logout';
 
 
-  constructor(private snackbar: MatSnackBar, private http: HttpClient) { }
+  constructor(private snackbar: MatSnackBar, private http: HttpClient,
+              private router: Router,) { }
 
   showMessage(msg: string): void {
     this.snackbar.open(msg, 'X', {
@@ -22,9 +25,13 @@ export class LogoutService {
     });
   }
 
-  sair(): Observable<any> {
+  sair(): void {
     const url = `${this.baseUrl}`;
-    return this.http.get<any>(url);
+    this.http.get<any>(url).subscribe(
+      resultado => {
+        localStorage.setItem('logado', 'false');
+        localStorage.setItem('token', '');
+        this.router.navigate(['/login']);
+      })
   }
-
 }
