365db7239cf3b6fb0db606438ef9fe3506ff1238
Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>
2022-05-27 17:16:59 -0300

    craindo constatantes para comparacao de tempo para se caso tiver tempo inativo ele irá deslogar
    Signed-off-by: Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>

--- src/app/components/service/interceptor.service.ts
+++ src/app/components/service/interceptor.service.ts
@@ -1,3 +1,4 @@
+import { LogoutService } from './logout.service';
 import {
   HttpEvent,
   HttpInterceptor,
@@ -17,6 +18,9 @@ export class Interceptor implements HttpInterceptor {
   token: String = '';
   serie = 2001;
 
+  constructor(private logoutService: LogoutService) { }
+
+
  intercept( request: HttpRequest<any>, next: HttpHandler ):
  Observable<HttpEvent<any>> {
 
@@ -27,11 +31,21 @@ export class Interceptor implements HttpInterceptor {
     const token_str = this.storage.getItem('token') ? this.storage.getItem('token') : '';
     this.password = new String(password_str);
     this.user = new String(user_str);
+    console.table(this.storage.getItem('token'));
     this.token = new String(token_str);
+
+    const timeout = new Date(this.storage.getItem('timeoutAngular') as string);
+    const timeoutnow = new Date();
+    const logado = this.storage.getItem('logado');
+
+    if((logado == 'true') && (timeout <= timeoutnow)){
+      this.logoutService.sair();
+    }
 
     if(this.token.length > 1){
       headers = headers.append('token', token_str as string)
-      console.log('Autorizado por token')
+      console.log('Autorizado por token');
+      console.table(token_str);
     }
     else {
       if ((this.user.length > 1) && ( this.password.length > 1)){
