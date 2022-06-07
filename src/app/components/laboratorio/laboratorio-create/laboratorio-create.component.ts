0740090ff3d78611ab00fb707ed0f2a69b54b5d9
RogerDelNobre <rogerdelnobre@gmail.com>
2022-06-07 17:10:08 -0300

    Refatoracao do componente

--- src/app/components/laboratorio/laboratorio-create/laboratorio-create.component.ts
+++ src/app/components/laboratorio/laboratorio-create/laboratorio-create.component.ts
@@ -6,11 +6,24 @@ import { Router } from '@angular/router';
 import { LaboratorioService } from '../../service/laboratorio.service';
 import { STRING_TYPE } from '@angular/compiler';
 import { pipe, map } from 'rxjs';
-import { AfterViewInit, ElementRef, ViewChild, Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ElementRef,
+  ViewChild,
+  Component,
+  OnInit,
+} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { debounceTime, distinctUntilChanged, startWith, tap, delay, filter } from 'rxjs/operators';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  startWith,
+  tap,
+  delay,
+  filter,
+} from 'rxjs/operators';
 import { merge, fromEvent } from 'rxjs';
 import { Subject } from 'rxjs';
 
@@ -34,28 +47,29 @@ export class LaboratorioCreateComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    const query = new Query({ key: '', value: '', isNumeric: false })
+    const query = new Query({ key: '', value: '', isNumeric: false });
 
-    this.subject
-      .pipe(debounceTime(500))
-      .subscribe(() => {
-        this.laboratoryDomainService
-          .findLaboratoryDomains('id', 'asc', 0, 60, this.queries)
-
-          .subscribe((laboratoryDomains) => {
-            console.table(this.queries);
-            this.laboratoryDomains = laboratoryDomains;
-          });
-      });
+    this.subject.pipe(debounceTime(500)).subscribe(() => {
+      this.laboratoryDomainService
+        .findLaboratoryDomains('id', 'asc', 0, 60, this.queries)
+        .subscribe((laboratoryDomains) => {
+          console.table(this.queries);
+          this.laboratoryDomains = laboratoryDomains;
+        });
+    });
     this.subject.next(null);
   }
 
   search(): void {
-    const query_string = this.laboratorio.laboratory_domain_id as unknown as string;
-    const query = new Query({ key: 'name', value: query_string, isNumeric: false });
+    const query_string = this.laboratorio
+      .laboratory_domain_id as unknown as string;
+    const query = new Query({
+      key: 'name',
+      value: query_string,
+      isNumeric: false,
+    });
     this.queries = [];
     this.queries.push(query);
-
     this.subject.next(null);
   }
 
@@ -74,9 +88,10 @@ export class LaboratorioCreateComponent implements OnInit {
 
   displayFn(options: LaboratoryDomain[]): (id: any) => any {
     return (id: any) => {
-      const correspondingOption = Array.isArray(options) ? options.find(option => option.id === id) : null;
+      const correspondingOption = Array.isArray(options)
+        ? options.find((option) => option.id === id)
+        : null;
       return correspondingOption ? correspondingOption.name : '';
-    }
+    };
   }
-
 }
