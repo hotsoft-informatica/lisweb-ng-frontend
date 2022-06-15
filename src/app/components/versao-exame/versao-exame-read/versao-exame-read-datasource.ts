58bcf3a687e31b631de928eddaa8ea425fd64d26
RogerDelNobre <rogerdelnobre@gmail.com>
2022-06-15 10:47:11 -0300

    Cria datasource de versao exame

---
+++ src/app/components/versao-exame/versao-exame-read/versao-exame-read-datasource.ts
@@ -0,0 +1,47 @@
+import { VersaoExame } from '../../model/versao-exame.model';
+import { VersaoExameService } from '../../service/versao-exame.service';
+import { CollectionViewer, DataSource } from '@angular/cdk/collections';
+import { Observable, BehaviorSubject, of } from 'rxjs';
+import { catchError, finalize } from 'rxjs/operators';
+import { Query } from '../../model/query.model';
+
+export class VersaoExameReadDataSource implements DataSource<VersaoExame> {
+  private versaoExamesSubject = new BehaviorSubject<VersaoExame[]>([]);
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+
+  query: Query[] = [];
+
+  public loading$ = this.loadingSubject.asObservable();
+
+  constructor(private versaoExameService: VersaoExameService) { }
+
+  loadVersaoExames(
+    active: string,
+    sortDirection: string,
+    pageIndex: number,
+    pageSize: number,
+    query: Query[] | null
+  ) {
+    this.loadingSubject.next(true);
+
+    this.versaoExameService
+      .findVersaoExames(active, sortDirection, pageIndex, pageSize, query)
+      .pipe(
+        catchError(() => of([])),
+        finalize(() => this.loadingSubject.next(false))
+      )
+      .subscribe((versaoExames: VersaoExame[]) =>
+        this.versaoExamesSubject.next(versaoExames)
+      );
+  }
+
+  connect(collectionViewer: CollectionViewer): Observable<VersaoExame[]> {
+    console.log('Conectando ao data source');
+    return this.versaoExamesSubject.asObservable();
+  }
+
+  disconnect(collectionViewer: CollectionViewer): void {
+    this.versaoExamesSubject.complete();
+    this.loadingSubject.complete();
+  }
+}
