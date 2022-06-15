c6fb23a9e0eb8bb3922b941aa95e2939a0bd3356
RogerDelNobre <rogerdelnobre@gmail.com>
2022-06-15 15:33:50 -0300

    Cria view crud component de versao exame

---
+++ src/app/views/versao-exame-crud/versao-exame-crud.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-versao-exame-crud',
+  templateUrl: './versao-exame-crud.component.html',
+  styleUrls: ['./versao-exame-crud.component.css']
+})
+export class VersaoExameCrudComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  navigateToVersaoExameCreate(): void {
+    this.router.navigate(['/versao-exame/create']);
+  }
+
+}
