8c3a871088bcece255e3550496de10c5ea44fd90
RogerDelNobre <rogerdelnobre@gmail.com>
2022-06-15 10:50:49 -0300

    Cria componente de versao exame

--- src/app/components/versao-exame/versao-exame-read/versao-exame-read.component.ts
+++ src/app/components/versao-exame/versao-exame-read/versao-exame-read.component.ts
@@ -1,15 +1,87 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Query } from '../../model/query.model';
+import { VersaoExame } from '../../model/versao-exame.model';
+import { VersaoExameService } from '../../service/versao-exame.service';
+import { VersaoExameReadDataSource } from './versao-exame-read-datasource';
+import {
+  AfterViewInit,
+  ElementRef,
+  ViewChild,
+  Component,
+  OnInit,
+} from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  startWith,
+  tap,
+  delay,
+  filter,
+} from 'rxjs/operators';
+import { merge, fromEvent } from 'rxjs';
 @Component({
   selector: 'app-versao-exame-read',
   templateUrl: './versao-exame-read.component.html',
   styleUrls: ['./versao-exame-read.component.css']
 })
-export class VersaoExameReadComponent implements OnInit {
+export class VersaoExameReadComponent implements OnInit, AfterViewInit {
+  totalCount!: number;
+  dataSource!: VersaoExameReadDataSource;
 
-  constructor() { }
+  displayedColumns = [
+    'id',
+    'laboratorio_id',
+    'versao',
+    'agrupamento',
+    'descricao',
+    'volume',
+    'status',
+    'aprovado',
+    'created_at',
+    'action',
+  ];
 
-  ngOnInit(): void {
+  @ViewChild(MatPaginator) paginator: MatPaginator | any;
+
+  @ViewChild(MatSort) sort: MatSort | any;
+
+  query: Query[] = [];
+
+  constructor(private versaoExameService: VersaoExameService) { }
+
+  search(key: string, value: string, isNumeric: boolean = false): void {
+    const query = new Query({ key, value, isNumeric });
+    this.query = this.query.filter((q) => q.key !== key);
+    this.query.push(query);
+    this.paginator.pageIndex = 0;
+    this.loadVersaoExamesPage();
   }
 
+  ngOnInit(): void {
+    this.dataSource = new VersaoExameReadDataSource(this.versaoExameService);
+    this.dataSource.loadVersaoExames('id', 'desc', 1, 10, null);
+    this.versaoExameService.countVersaoExames().subscribe((totalCount) => {
+      this.totalCount = totalCount;
+    });
+  }
+
+  ngAfterViewInit() {
+    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0)); // reseta o paginador depois de ordenar
+
+    merge(this.sort.sortChange, this.paginator.page) // Na ordenação ou paginação, carrega uma nova página
+      .pipe(tap(() => this.loadVersaoExamesPage()))
+      .subscribe();
+  }
+
+  loadVersaoExamesPage() {
+    this.dataSource.loadVersaoExames(
+      this.sort.active,
+      this.sort.direction,
+      this.paginator.pageIndex,
+      this.paginator.pageSize,
+      this.query
+    );
+  }
 }
