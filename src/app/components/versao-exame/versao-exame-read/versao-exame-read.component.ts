317df1901d8737deff3370ae186335c7300467ae
RogerDelNobre <rogerdelnobre@gmail.com>
2022-06-27 15:59:29 -0300

    Implementa delete de versao exame component

--- src/app/components/versao-exame/versao-exame-read/versao-exame-read.component.ts
+++ src/app/components/versao-exame/versao-exame-read/versao-exame-read.component.ts
@@ -1,4 +1,6 @@
+import { MatDialog } from '@angular/material/dialog';
 import { Query } from '../../model/query.model';
+import { ActivatedRoute, Router } from '@angular/router';
 import { VersaoExame } from '../../model/versao-exame.model';
 import { VersaoExameService } from '../../service/versao-exame.service';
 import { VersaoExameReadDataSource } from './versao-exame-read-datasource';
@@ -8,6 +10,7 @@ import {
   ViewChild,
   Component,
   OnInit,
+  TemplateRef,
 } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
@@ -24,26 +27,28 @@ import { merge, fromEvent } from 'rxjs';
 @Component({
   selector: 'app-versao-exame-read',
   templateUrl: './versao-exame-read.component.html',
-  styleUrls: ['./versao-exame-read.component.css']
+  styleUrls: ['./versao-exame-read.component.css'],
 })
 export class VersaoExameReadComponent implements OnInit, AfterViewInit {
   totalCount!: number;
   dataSource!: VersaoExameReadDataSource;
 
-  displayedColumns = [
-    'titulo_laudo',
-    'descricao',
-    'status',
-    'action',
-  ];
+  displayedColumns = ['titulo_laudo', 'descricao', 'status', 'action'];
 
   @ViewChild(MatPaginator) paginator: MatPaginator | any;
 
   @ViewChild(MatSort) sort: MatSort | any;
 
+  @ViewChild('deleteDialog') deleteDialog: TemplateRef<any> | any;
+
   query: Query[] = [];
 
-  constructor(private versaoExameService: VersaoExameService) { }
+  constructor(
+    private versaoExameService: VersaoExameService,
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   search(key: string, value: string, isNumeric: boolean = false): void {
     const query = new Query({ key, value, isNumeric });
@@ -67,6 +72,22 @@ export class VersaoExameReadComponent implements OnInit, AfterViewInit {
     merge(this.sort.sortChange, this.paginator.page) // Na ordenação ou paginação, carrega uma nova página
       .pipe(tap(() => this.loadVersaoExamesPage()))
       .subscribe();
+  }
+
+  deleteVersaoExame(id: number): void {
+    const dialogRef = this.dialog.open(this.deleteDialog);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.versaoExameService
+          .delete(id)
+          .subscribe(() => {
+            this.router.navigate(['/versao_exames']).then(() => {
+              window.location.reload();
+            });
+          });
+      }
+    });
   }
 
   loadVersaoExamesPage() {
