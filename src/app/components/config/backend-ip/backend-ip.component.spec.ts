c5664cd17b41533a925b6d2fb86c7cbf0657681f
RogerDelNobre <rogerdelnobre@gmail.com>
2022-06-28 17:04:58 -0300

    Cria componente config e backend-id

---
+++ src/app/components/config/backend-ip/backend-ip.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BackendIpComponent } from './backend-ip.component';
+
+describe('BackendIpComponent', () => {
+  let component: BackendIpComponent;
+  let fixture: ComponentFixture<BackendIpComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BackendIpComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BackendIpComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/config/backend-ip/backend-ip.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-backend-ip',
+  templateUrl: './backend-ip.component.html',
+  styleUrls: ['./backend-ip.component.css']
+})
+export class BackendIpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
