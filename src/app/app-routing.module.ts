f27acf27f8b2ac0db1ec801437af6268dd38dd67
RogerDelNobre <rogerdelnobre@gmail.com>
2022-06-30 17:03:47 -0300

    Configura reload quando mesma pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -422,7 +422,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    onSameUrlNavigation: 'reload'
+  })],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
