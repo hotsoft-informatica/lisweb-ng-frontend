4fab3376221533bc3414fbf1fbcb5a8329ac9170
Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>
2022-07-12 17:19:25 -0300

    adicionado componente para criar metodo direto
    Signed-off-by: Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>

--- src/app/components/template/header/header.component.ts
+++ src/app/components/template/header/header.component.ts
@@ -1,7 +1,10 @@
+import { UsuarioCreateComponent } from './../../usuario/usuario-create/usuario-create.component';
+import { MetodoExameComponent } from './../../metodo-exame/metodo-exame.component';
 import { LogoutService } from './../../service/logout.service';
 import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from '../../service/usuario.service';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-header',
@@ -13,7 +16,8 @@ export class HeaderComponent implements OnInit {
 
   constructor(private usuarioService: UsuarioService,
     private router: Router,
-    private logoutService: LogoutService) { }
+    private logoutService: LogoutService,
+    public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.routerStr = window.location.href;
@@ -25,5 +29,15 @@ export class HeaderComponent implements OnInit {
 
   logout(): void {
     this.logoutService.sair();
+  }
+
+  openDialogCreate(): void {
+    const dialogRef = this.dialog.open(MetodoExameComponent, {
+      width: '750px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 }
