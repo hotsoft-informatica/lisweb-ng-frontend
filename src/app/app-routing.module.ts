2848fc38450e9dc4eef3440544d7e265aa0dbfe6
Wellington Torrejais da Silva <wtds.trabalho@gmail.com>
2022-07-19 19:12:37 -0300

    Merge rotas
    Signed-off-by: Wellington Torrejais da Silva <wtds.trabalho@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,90 +1,89 @@
-import { LocalDeAtendimentoReadComponent } from './components/local-de-atendimento/local-de-atendimento-read/local-de-atendimento-read.component';
-import { LocalDeAtendimentoCreateComponent } from './components/local-de-atendimento/local-de-atendimento-create/local-de-atendimento-create.component';
-import { UsuarioReadComponent } from './components/usuario/usuario-read/usuario-read.component';
-import { LgSenhaComponent } from './components/login/lg-senha/lg-senha.component';
-import { LoginComponent } from './components/login/login.component';
-import { PacienteShowComponent } from './components/paciente/paciente-show/paciente-show.component';
-import { PacienteReadComponent } from './components/paciente/paciente-read/paciente-read.component';
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AmostraCreateComponent } from './components/amostra/amostra-create/amostra-create.component';
+import { AmostraDeleteComponent } from './components/amostra/amostra-delete/amostra-delete.component';
+import { AmostraUpdateComponent } from './components/amostra/amostra-update/amostra-update.component';
+import { BackendIpComponent } from './components/config/backend-ip/backend-ip.component';
+import { BancadaCreateComponent } from './components/bancada/bancada-create/bancada-create.component';
+import { BancadaDeleteComponent } from './components/bancada/bancada-delete/bancada-delete.component';
+import { BancadaUpdateComponent } from './components/bancada/bancada-update/bancada-update.component';
+import { BandejaAmostraCreateComponent } from './components/bandeja-amostra/bandeja-amostra-create/bandeja-amostra-create.component';
+import { BandejaAmostraDeleteComponent } from './components/bandeja-amostra/bandeja-amostra-delete/bandeja-amostra-delete.component';
+import { BandejaAmostraUpdateComponent } from './components/bandeja-amostra/bandeja-amostra-update/bandeja-amostra-update.component';
+import { BandejaCreateComponent } from './components/bandeja/bandeja-create/bandeja-create.component';
+import { BandejaDeleteComponent } from './components/bandeja/bandeja-delete/bandeja-delete.component';
+import { BandejaUpdateComponent } from './components/bandeja/bandeja-update/bandeja-update.component';
+import { ColetorCreateComponent } from './components/coletor/coletor-create/coletor-create.component';
+import { ColetorDeleteComponent } from './components/coletor/coletor-delete/coletor-delete.component';
+import { ColetorUpdateComponent } from './components/coletor/coletor-update/coletor-update.component';
+import { ConsultaAmostraShowComponent } from './components/consulta-amostra/consulta-amostra-show/consulta-amostra-show.component';
+import { EmpresaCreateComponent } from './components/empresa/empresa-create/empresa-create.component';
+import { EmpresaCrudComponent } from './views/empresa-crud/empresa-crud.component';
+import { EmpresaDeleteComponent } from './components/empresa/empresa-delete/empresa-delete.component';
+import { EmpresaShowComponent } from './components/empresa/empresa-show/empresa-show.component';
+import { EmpresaUpdateComponent } from './components/empresa/empresa-update/empresa-update.component';
+import { ExameAmostraCreateComponent } from './components/exame-amostra/exame-amostra-create/exame-amostra-create.component';
+import { ExameAmostraDeleteComponent } from './components/exame-amostra/exame-amostra-delete/exame-amostra-delete.component';
+import { ExameAmostraUpdateComponent } from './components/exame-amostra/exame-amostra-update/exame-amostra-update.component';
+import { ExameCreateComponent } from './components/exame/exame-create/exame-create.component';
+import { ExameDeleteComponent } from './components/exame/exame-delete/exame-delete.component';
+import { ExameUpdateComponent } from './components/exame/exame-update/exame-update.component';
 import { HomeComponent } from './views/home/home.component';
-import { LaboratoryDomainCrudComponent } from './views/laboratory-domain-crud/laboratory-domain-crud.component';
-import { LaboratoryDomainCreateComponent } from './components/laboratory-domain/laboratory-domain-create/laboratory-domain-create.component';
-import { LaboratoryDomainUpdateComponent } from './components/laboratory-domain/laboratory-domain-update/laboratory-domain-update.component';
-import { LaboratoryDomainDeleteComponent } from './components/laboratory-domain/laboratory-domain-delete/laboratory-domain-delete.component';
-import { LaboratorioCrudComponent } from './views/laboratorio-crud/laboratorio-crud.component';
 import { LaboratorioCreateComponent } from './components/laboratorio/laboratorio-create/laboratorio-create.component';
-import { LaboratorioUpdateComponent } from './components/laboratorio/laboratorio-update/laboratorio-update.component';
+import { LaboratorioCrudComponent } from './views/laboratorio-crud/laboratorio-crud.component';
 import { LaboratorioDeleteComponent } from './components/laboratorio/laboratorio-delete/laboratorio-delete.component';
-import { PacienteCreateComponent } from './components/paciente/paciente-create/paciente-create.component';
-import { PacienteUpdateComponent } from './components/paciente/paciente-update/paciente-update.component';
-import { PacienteDeleteComponent } from './components/paciente/paciente-delete/paciente-delete.component';
-import { RequisicaoCrudComponent } from './views/requisicao-crud/requisicao-crud.component';
-import { RequisicaoCreateComponent } from './components/requisicao/requisicao-create/requisicao-create.component';
-import { RequisicaoShowComponent } from './components/requisicao/requisicao-show/requisicao-show.component';
-import { RequisicaoUpdateComponent } from './components/requisicao/requisicao-update/requisicao-update.component';
-import { RequisicaoDeleteComponent } from './components/requisicao/requisicao-delete/requisicao-delete.component';
-import { UsuarioCreateComponent } from './components/usuario/usuario-create/usuario-create.component';
-import { UsuarioUpdateComponent } from './components/usuario/usuario-update/usuario-update.component';
-import { UsuarioDeleteComponent } from './components/usuario/usuario-delete/usuario-delete.component';
-import { TipoExameCreateComponent } from './components/tipo-exame/tipo-exame-create/tipo-exame-create.component';
-import { TipoExameUpdateComponent } from './components/tipo-exame/tipo-exame-update/tipo-exame-update.component';
-import { TipoExameDeleteComponent } from './components/tipo-exame/tipo-exame-delete/tipo-exame-delete.component';
-import { ValorReferenciaReadComponent } from './components/valor-referencia/valor-referencia-read/valor-referencia-read/valor-referencia-read.component';
-import { VersaoExameCrudComponent } from './views/versao-exame-crud/versao-exame-crud.component';
-import { VersaoExameCreateComponent } from './components/versao-exame/versao-exame-create/versao-exame-create.component';
-import { VersaoExameUpdateComponent } from './components/versao-exame/versao-exame-update/versao-exame-update.component';
-import { VersaoExameDeleteComponent } from './components/versao-exame/versao-exame-delete/versao-exame-delete.component';
-import { MaterialBiologicoCreateComponent } from './components/material-biologico/material-biologico-create/material-biologico-create.component';
-import { MaterialBiologicoUpdateComponent } from './components/material-biologico/material-biologico-update/material-biologico-update.component';
-import { MaterialBiologicoDeleteComponent } from './components/material-biologico/material-biologico-delete/material-biologico-delete.component';
-import { MarcacaoReadComponent } from './components/marcacao/marcacao-read/marcacao-read.component';
+import { LaboratorioUpdateComponent } from './components/laboratorio/laboratorio-update/laboratorio-update.component';
+import { LaboratoryDomainCreateComponent } from './components/laboratory-domain/laboratory-domain-create/laboratory-domain-create.component';
+import { LaboratoryDomainCrudComponent } from './views/laboratory-domain-crud/laboratory-domain-crud.component';
+import { LaboratoryDomainDeleteComponent } from './components/laboratory-domain/laboratory-domain-delete/laboratory-domain-delete.component';
+import { LaboratoryDomainUpdateComponent } from './components/laboratory-domain/laboratory-domain-update/laboratory-domain-update.component';
+import { LgSenhaComponent } from './components/login/lg-senha/lg-senha.component';
+import { LocalDeAtendimentoCreateComponent } from './components/local-de-atendimento/local-de-atendimento-create/local-de-atendimento-create.component';
+import { LocalDeAtendimentoReadComponent } from './components/local-de-atendimento/local-de-atendimento-read/local-de-atendimento-read.component';
+import { LoginComponent } from './components/login/login.component';
+import { LoteAmostraCreateComponent } from './components/lote-amostra/lote-amostra-create/lote-amostra-create.component';
+import { LoteAmostraDeleteComponent } from './components/lote-amostra/lote-amostra-delete/lote-amostra-delete.component';
+import { LoteAmostraUpdateComponent } from './components/lote-amostra/lote-amostra-update/lote-amostra-update.component';
 import { MarcacaoCreateComponent } from './components/marcacao/marcacao-create/marcacao-create.component';
 import { MarcacaoDeleteComponent } from './components/marcacao/marcacao-delete/marcacao-delete.component';
-import { LoteAmostraCreateComponent } from './components/lote-amostra/lote-amostra-create/lote-amostra-create.component';
-import { LoteAmostraUpdateComponent } from './components/lote-amostra/lote-amostra-update/lote-amostra-update.component';
-import { LoteAmostraDeleteComponent } from './components/lote-amostra/lote-amostra-delete/lote-amostra-delete.component';
-import { ExameAmostraCreateComponent } from './components/exame-amostra/exame-amostra-create/exame-amostra-create.component';
-import { ExameAmostraUpdateComponent } from './components/exame-amostra/exame-amostra-update/exame-amostra-update.component';
-import { ExameAmostraDeleteComponent } from './components/exame-amostra/exame-amostra-delete/exame-amostra-delete.component';
-import { ExameCreateComponent } from './components/exame/exame-create/exame-create.component';
-import { ExameUpdateComponent } from './components/exame/exame-update/exame-update.component';
-import { ExameDeleteComponent } from './components/exame/exame-delete/exame-delete.component';
-import { EmpresaCrudComponent } from './views/empresa-crud/empresa-crud.component';
-import { EmpresaCreateComponent } from './components/empresa/empresa-create/empresa-create.component';
-import { EmpresaDeleteComponent } from './components/empresa/empresa-delete/empresa-delete.component';
-import { EmpresaUpdateComponent } from './components/empresa/empresa-update/empresa-update.component';
-import { EmpresaShowComponent } from './components/empresa/empresa-show/empresa-show.component';
-import { ColetorCreateComponent } from './components/coletor/coletor-create/coletor-create.component';
-import { ColetorUpdateComponent } from './components/coletor/coletor-update/coletor-update.component';
-import { ColetorDeleteComponent } from './components/coletor/coletor-delete/coletor-delete.component';
-import { BandejaAmostraCreateComponent } from './components/bandeja-amostra/bandeja-amostra-create/bandeja-amostra-create.component';
-import { BandejaAmostraUpdateComponent } from './components/bandeja-amostra/bandeja-amostra-update/bandeja-amostra-update.component';
-import { BandejaAmostraDeleteComponent } from './components/bandeja-amostra/bandeja-amostra-delete/bandeja-amostra-delete.component';
-import { BandejaCreateComponent } from './components/bandeja/bandeja-create/bandeja-create.component';
-import { BandejaUpdateComponent } from './components/bandeja/bandeja-update/bandeja-update.component';
-import { BandejaDeleteComponent } from './components/bandeja/bandeja-delete/bandeja-delete.component';
-import { BancadaCreateComponent } from './components/bancada/bancada-create/bancada-create.component';
-import { BancadaUpdateComponent } from './components/bancada/bancada-update/bancada-update.component';
-import { BancadaDeleteComponent } from './components/bancada/bancada-delete/bancada-delete.component';
-import { AmostraCreateComponent } from './components/amostra/amostra-create/amostra-create.component';
-import { AmostraUpdateComponent } from './components/amostra/amostra-update/amostra-update.component';
-import { AmostraDeleteComponent } from './components/amostra/amostra-delete/amostra-delete.component';
-import { OperadoraCrudComponent } from './views/operadora-crud/operadora-crud.component';
+import { MarcacaoReadComponent } from './components/marcacao/marcacao-read/marcacao-read.component';
+import { MaterialBiologicoCreateComponent } from './components/material-biologico/material-biologico-create/material-biologico-create.component';
+import { MaterialBiologicoDeleteComponent } from './components/material-biologico/material-biologico-delete/material-biologico-delete.component';
+import { MaterialBiologicoUpdateComponent } from './components/material-biologico/material-biologico-update/material-biologico-update.component';
 import { OperadoraCreateComponent } from './components/operadora/operadora-create/operadora-create.component';
-import { OperadoraUpdateComponent } from './components/operadora/operadora-update/operadora-update.component';
+import { OperadoraCrudComponent } from './views/operadora-crud/operadora-crud.component';
 import { OperadoraDeleteComponent } from './components/operadora/operadora-delete/operadora-delete.component';
 import { OperadoraShowComponent } from './components/operadora/operadora-show/operadora-show.component';
-import { ConsultaAmostraShowComponent } from './components/consulta-amostra/consulta-amostra-show/consulta-amostra-show.component';
-import { UploadFileComponent } from './components/upload-file/upload-file.component';
+import { OperadoraUpdateComponent } from './components/operadora/operadora-update/operadora-update.component';
+import { PacienteCreateComponent } from './components/paciente/paciente-create/paciente-create.component';
+import { PacienteDeleteComponent } from './components/paciente/paciente-delete/paciente-delete.component';
+import { PacienteReadComponent } from './components/paciente/paciente-read/paciente-read.component';
+import { PacienteShowComponent } from './components/paciente/paciente-show/paciente-show.component';
+import { PacienteUpdateComponent } from './components/paciente/paciente-update/paciente-update.component';
+import { RequisicaoCreateComponent } from './components/requisicao/requisicao-create/requisicao-create.component';
+import { RequisicaoCrudComponent } from './views/requisicao-crud/requisicao-crud.component';
+import { RequisicaoDeleteComponent } from './components/requisicao/requisicao-delete/requisicao-delete.component';
+import { RequisicaoShowComponent } from './components/requisicao/requisicao-show/requisicao-show.component';
+import { RequisicaoUpdateComponent } from './components/requisicao/requisicao-update/requisicao-update.component';
+import { ResponsavelTecnicoCreateComponent } from './components/responsavel-tecnico/responsavel-tecnico-create/responsavel-tecnico-create.component';
 import { ResponsavelTecnicoCrudComponent } from './views/responsavel-tecnico-crud/responsavel-tecnico-crud.component';
 import { ResponsavelTecnicoReadComponent } from './components/responsavel-tecnico/responsavel-tecnico-read/responsavel-tecnico-read.component';
-import { ResponsavelTecnicoCreateComponent } from './components/responsavel-tecnico/responsavel-tecnico-create/responsavel-tecnico-create.component';
-import { ResponsavelTecnicoUploadComponent } from './components/responsavel-tecnico/responsavel-tecnico-upload/responsavel-tecnico-upload.component';
-import { ResponsavelTecnicoUpdateComponent } from './components/responsavel-tecnico/responsavel-tecnico-update/responsavel-tecnico-update.component';
 import { ResponsavelTecnicoShowComponent } from './components/responsavel-tecnico/responsavel-tecnico-show/responsavel-tecnico-show.component';
-import { BackendIpComponent } from './components/config/backend-ip/backend-ip.component';
+import { ResponsavelTecnicoUpdateComponent } from './components/responsavel-tecnico/responsavel-tecnico-update/responsavel-tecnico-update.component';
+import { ResponsavelTecnicoUploadComponent } from './components/responsavel-tecnico/responsavel-tecnico-upload/responsavel-tecnico-upload.component';
+import { TipoExameCreateComponent } from './components/tipo-exame/tipo-exame-create/tipo-exame-create.component';
+import { TipoExameDeleteComponent } from './components/tipo-exame/tipo-exame-delete/tipo-exame-delete.component';
+import { TipoExameUpdateComponent } from './components/tipo-exame/tipo-exame-update/tipo-exame-update.component';
+import { UploadFileComponent } from './components/upload-file/upload-file.component';
+import { UsuarioCreateComponent } from './components/usuario/usuario-create/usuario-create.component';
+import { UsuarioDeleteComponent } from './components/usuario/usuario-delete/usuario-delete.component';
+import { UsuarioReadComponent } from './components/usuario/usuario-read/usuario-read.component';
+import { UsuarioUpdateComponent } from './components/usuario/usuario-update/usuario-update.component';
+import { ValorReferenciaReadComponent } from './components/valor-referencia/valor-referencia-read/valor-referencia-read/valor-referencia-read.component';
+import { VersaoExameCreateComponent } from './components/versao-exame/versao-exame-create/versao-exame-create.component';
+import { VersaoExameCrudComponent } from './views/versao-exame-crud/versao-exame-crud.component';
+import { VersaoExameDeleteComponent } from './components/versao-exame/versao-exame-delete/versao-exame-delete.component';
 
 const routes: Routes = [
   {
@@ -136,7 +135,7 @@ const routes: Routes = [
     component: PacienteReadComponent,
   },
   {
-    path: 'pacientes/show:id',
+    path: 'pacientes/show/:id',
     component: PacienteReadComponent,
   },
   {
@@ -203,7 +202,6 @@ const routes: Routes = [
     path: 'tipo-exames/delete/:id',
     component: TipoExameDeleteComponent,
   },
-  ,
   {
     path: 'valores_referencia/read',
     component: ValorReferenciaReadComponent,
@@ -219,7 +217,7 @@ const routes: Routes = [
   {
     path: 'valores_referencia/delete/:id',
     component: ValorReferenciaReadComponent,
-  }
+  },
   {
     path: 'versao_exames',
     component: VersaoExameCrudComponent,
@@ -455,13 +453,14 @@ const routes: Routes = [
   {
     path: 'backend_ip/create',
     component: BackendIpComponent,
-  }
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    onSameUrlNavigation: 'reload'
-  })],
+  imports: [
+    RouterModule.forRoot(routes, {
+      onSameUrlNavigation: 'reload'
+    })],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
