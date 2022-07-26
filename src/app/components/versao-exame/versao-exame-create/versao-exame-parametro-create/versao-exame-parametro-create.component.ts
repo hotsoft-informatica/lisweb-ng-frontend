b1ca35ae00d776a4fdd59bc93b3cdade3248a9ab
RogerDelNobre <rogerdelnobre@gmail.com>
2022-07-26 16:13:56 -0300

    Cria versao exame parametro create component

---
+++ src/app/components/versao-exame/versao-exame-create/versao-exame-parametro-create/versao-exame-parametro-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-versao-exame-parametro-create',
+  templateUrl: './versao-exame-parametro-create.component.html',
+  styleUrls: ['./versao-exame-parametro-create.component.css']
+})
+export class VersaoExameParametroCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
