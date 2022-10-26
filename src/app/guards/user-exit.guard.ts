20e4344a14bd056b8098e837f191e01230b3fd79
RogerDelNobre <rogerdelnobre@gmail.com>
2022-10-26 17:00:16 -0300

    Cria estrutura de guards

---
+++ src/app/guards/user-exit.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { UserComponent } from '../components/user/user.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserExitGuard implements CanDeactivate<unknown> {
+  canDeactivate(
+    component: unknown,
+    currentRoute: ActivatedRouteSnapshot,
+    currentState: RouterStateSnapshot,
+    nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+
+}
