dc88c1438cdc85ab974b33579b8ab5470f9ad728
RogerDelNobre <rogerdelnobre@gmail.com>
2022-10-26 15:20:22 -0300

    Adiciona alert e ajusta parametro de nevegacao em rotas de user

--- src/app/user.guard.ts
+++ src/app/user.guard.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserGuard implements CanActivate {
+  constructor(private router: Router) { }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+    alert('Sem autorização para acessar a rota.');
+    this.router.navigate(['']);
+    return false;
   }
-  
+
 }
