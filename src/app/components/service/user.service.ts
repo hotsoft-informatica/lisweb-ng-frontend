9329e027d5fceb59979fa040ff44ce2be8f8c387
RogerDelNobre <rogerdelnobre@gmail.com>
2022-11-11 08:48:33 -0300

    Cria servide de User

---
+++ src/app/components/service/user.service.ts
@@ -0,0 +1,84 @@
+import { User } from '../model/user.model';
+import { Query } from './../model/query.model';
+import { Injectable } from '@angular/core';
+import { BackendIpService } from './backend-ip.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  baseUrl = '/users';
+
+  query: Query[] = [];
+
+  constructor(
+    private snackbar: MatSnackBar,
+    private http: HttpClient,
+    private backendIpService: BackendIpService
+  ) {
+    this.baseUrl = backendIpService.getUrl() + this.baseUrl;
+  }
+
+  showMessage(msg: string): void {
+    this.snackbar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+    });
+  }
+
+  create(user: User): Observable<User> {
+    return this.http.post<User>(this.baseUrl, user);
+  }
+
+  read(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl);
+  }
+
+  readById(id: number): Observable<User> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.get<User>(url);
+  }
+
+  update(user: User): Observable<User> {
+    const url = `${this.baseUrl}/${user.id}`;
+    return this.http.put<User>(url, user);
+  }
+
+  delete(id: number): Observable<User> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete<User>(url);
+  }
+
+  find(
+    active: string = '',
+    sortOrder: string = 'asc',
+    pageNumber: number = 1,
+    pageSize: number = 3,
+    query: Query[] | null
+  ): Observable<User[]> {
+    let params = new HttpParams()
+      .set('active', active)
+      .set('sortOrder', sortOrder)
+      .set('pageNumber', pageNumber.toString())
+      .set('pageSize', pageSize.toString());
+    query?.forEach((queryItem) => {
+      if (queryItem) {
+        const key = `queryItem[${queryItem.key}]`;
+        params = params.append(key, queryItem.value);
+      }
+    });
+
+    return this.http.get<User[]>(this.baseUrl, {
+      params,
+    });
+  }
+
+  countRegisters(): Observable<number> {
+    return this.http.get<number>(this.baseUrl, {
+      params: new HttpParams().set('totalCount', 'true'),
+    });
+  }
+}
