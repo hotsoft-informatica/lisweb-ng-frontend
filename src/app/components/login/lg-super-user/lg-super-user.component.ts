62860506e8db60c339eafe6b5e53e79510f649eb
RogerDelNobre <rogerdelnobre@gmail.com>
2022-12-08 17:04:51 -0300

    Implementa login de super user via authorization

--- src/app/components/login/lg-super-user/lg-super-user.component.ts
+++ src/app/components/login/lg-super-user/lg-super-user.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { SuperUserLogin } from '../../model/login.model';
+import { Router } from '@angular/router';
+import { SuperUserService } from '../../service/super-user.service';
+import { Input, Component, OnInit } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-lg-super-user',
@@ -6,10 +10,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lg-super-user.component.css']
 })
 export class LgSuperUserComponent implements OnInit {
+  @Input('login') login: SuperUserLogin;
+  storage: Storage = window.localStorage;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private superUserService: SuperUserService
+  ) {
+    this.login = new SuperUserLogin({ "super_user": {} });
+  }
 
   ngOnInit(): void {
+  }
+
+  SuperUserLogin(): void {
+    // TODO: Tratar deprecation do subscribe
+    this.superUserService.login(this.login).subscribe(
+      (res) => {
+        console.log("Arrow function user");
+        let header: HttpHeaders = res.headers;
+        let auth: string = header.get('Authorization') as string;
+        localStorage.setItem('Authorization', auth);
+      },
+      (err) => {
+        console.log("Arrow function err")
+        console.table(err);
+        // TODO: Tratar retornos com erro
+        this.superUserService.showMessage('Usuário logado com sucesso!');
+        this.router.navigate(['/']);
+      },
+      () => {
+        console.log("Arrow function Vazio");
+        this.superUserService.showMessage('Usuário logado com sucesso!');
+        this.router.navigate(['/']);
+      });
   }
 
 }
