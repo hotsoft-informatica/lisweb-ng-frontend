6fccaa80fe891c40fd2760db72e4f678d9e8cddb
RogerDelNobre <rogerdelnobre@gmail.com>
2022-12-15 09:47:49 -0300

    Refatora model de user com devise jwt

--- src/app/components/model/user.model.ts
+++ src/app/components/model/user.model.ts
@@ -1,25 +1,20 @@
 export class User {
   public id?: number;
-  public provider?: string;
-  public uid?: string;
+  public email?: string;
   public encrypted_password?: string;
-  public reset_password_token?: string;
-  public password?: string;
-  public reset_password_sent_at?: string;
-  public allow_password_change?: boolean;
-  public remember_created_at?: Date = new Date();
-  public confirmation_token?: string;
-  public confirmed_at?: Date = new Date();
-  public unconfirmed_email?: Date = new Date();
-  public sign_in_count?: number;
-  public current_sign_in_at?: Date = new Date();
-  public last_sign_in_at?: Date = new Date();
   public name?: string;
   public nickname?: string;
   public image?: string;
-  public email?: string;
   public admin?: boolean;
   public tokens?: string;
+  public reset_password_token?: string;
+  public reset_password_sent_at?: string;
+  public remember_created_at?: Date = new Date();
+  public sign_in_count?: number;
+  public current_sign_in_at?: Date = new Date();
+  public last_sign_in_at?: Date = new Date();
+  public current_sign_in_ip?: number;
+  public last_sign_in_ip?: number;
   public created_at?: Date = new Date();
   public updated_at?: Date = new Date();
   public laboratorio_id?: number;
@@ -27,8 +22,7 @@ export class User {
   public changed_by_lab_id?: number;
   public version_id?: number;
   public original_id?: number;
-  public current_sign_in_ip?: number;
-  public last_sign_in_ip?: number;
+  public jti?: string;
 
   constructor(values: User) {
     Object.assign(this, values);
