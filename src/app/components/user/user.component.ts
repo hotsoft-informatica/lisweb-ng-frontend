4ff0305e2137e114671d3d3da04673b9feb48d46
RogerDelNobre <rogerdelnobre@gmail.com>
2023-01-06 16:42:16 -0300

    Implementa componente e metodos de user

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,23 +1,143 @@
-import { Component, OnInit } from '@angular/core';
+import { Query } from '../model/query.model';
+import { Component, OnInit, AfterViewInit, ViewChild, TemplateRef, Renderer2, ElementRef } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { User } from '../model/user.model';
+import { UserService } from '../service/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subject, timer } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { tap, debounceTime } from 'rxjs/operators';
+import { merge, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  datasource = new MatTableDataSource<any>([]);
+  records: any[] = [];
+  record!: any;
+  oldRecord: any;
+  currentRecord: any;
+  deletedRecords: any[] = [];
+  query: Query[] = [];
+  id!: number;
+  totalCount!: number;
 
-  ngOnInit(): void {
+
+  @ViewChild('deleteDialog') deleteDialog: TemplateRef<any> | any;
+  @ViewChild(MatSort) sort: MatSort | any;
+  @ViewChild(MatPaginator) paginator: MatPaginator | any;
+
+  onEdit = false;
+  onCreate = false;
+
+  displayedColumns = ['name', 'email', 'admin', 'action'];
+
+  constructor(
+    public dialog: MatDialog,
+    private renderer: Renderer2,
+    private router: Router,
+    private route: ActivatedRoute,
+    private userService: UserService,
+  ) {
+    this.currentRecord = new User({});
+    this.record ||= new User({});
   }
 
-  canExit(): boolean {
-    if (confirm("Você quer sair da rota?")) {
-      return true;
-    } else {
-      return false;
-    }
+  ngOnInit(): void {
+    this.userService.countRegisters().subscribe((totalCount) => {
+      this.totalCount = totalCount;
+    });
+  }
+
+  ngAfterViewInit() {
+    this.loadPage();
+    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0)); // reseta o paginador depois de ordenar
+
+    merge(this.sort.sortChange, this.paginator.page) // Na ordenação ou paginação, carrega uma nova página
+      .pipe(tap(() => this.loadPage()))
+      .subscribe();
+  }
+
+  loadPage() {
+    this.userService
+      .find(this.sort.active,
+        this.sort.direction,
+        this.paginator.pageIndex,
+        this.paginator.pageSize, this.query
+      ).subscribe((records: any[]) => {
+        this.records = records;
+        this.datasource.data = [...this.records];
+      });
+  }
+
+  new(): void {
+    this.onCreate = true;
+  }
+
+  addGridData(): void {
+    console.table(this.currentRecord);
+    this.onCreate = false;
+    this.onEdit = false;
+    this.userService.create(this.currentRecord).subscribe((record) => {
+      this.records.unshift(record);
+      this.datasource.data = [...this.records];
+      this.userService.showMessage('Usuário criado com sucesso!');
+    });
+
+    this.currentRecord = new User({});
+  }
+
+  updateGridData(): void {
+    this.onCreate = false;
+    this.onEdit = false;
+    this.userService.update(this.currentRecord).subscribe(() => {
+      this.userService.showMessage('Usuário atualizado com sucesso!');
+    });
+
+    this.currentRecord = new User({});
+  }
+
+  atualizar(row: User): void {
+    this.currentRecord = row;
+    this.onCreate = false;
+    this.onEdit = true;
+  }
+
+  cancelar(): void {
+    this.onCreate = false;
+    this.onEdit = false;
+    Object.assign(this.currentRecord, this.oldRecord);
+    this.currentRecord = new User({});
+  }
+
+  deleteGridData(id: number): void {
+    const dialogRef = this.dialog.open(this.deleteDialog);
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.userService.delete(id)
+          .subscribe(() => {
+            this.userService.showMessage('Usuário apagado com sucesso!');
+
+            // Carrega os dados do backend e faz refresh do datasource
+            this.loadPage();
+            this.datasource.data = [...this.records];
+          });
+      }
+    });
+  }
+
+  search(key: string, value: string, isNumeric: boolean = false): void {
+    const query = new Query({ key, value, isNumeric });
+    this.query = this.query.filter((q) => q.key !== key);
+    this.query.push(query);
+    this.paginator.pageIndex = 0;
+    this.loadPage();
   }
 
 }
