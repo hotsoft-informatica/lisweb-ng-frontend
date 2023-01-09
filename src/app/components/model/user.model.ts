03dc26b9f11694e03a23ecb200c42f72c9801720
RogerDelNobre <rogerdelnobre@gmail.com>
2023-01-09 16:25:53 -0300

    Adiciona campos devise ao model de user

--- src/app/components/model/user.model.ts
+++ src/app/components/model/user.model.ts
@@ -2,6 +2,8 @@ export class User {
   public id?: number;
   public email?: string;
   public encrypted_password?: string;
+  public password?: string;
+  public password_confirmation?: string;
   public name?: string;
   public nickname?: string;
   public image?: string;
