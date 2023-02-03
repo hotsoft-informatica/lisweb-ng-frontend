3658ab58b936d808b1c579e98014588e7807f598
RogerDelNobre <rogerdelnobre@gmail.com>
2023-02-03 16:48:37 -0300

    Adiciona coluna domain id no model de User

--- src/app/components/model/user.model.ts
+++ src/app/components/model/user.model.ts
@@ -4,6 +4,7 @@ export class User {
   public name?: string;
   public admin?: boolean;
   public usuarios_ids?: number;
+  public laboratory_domain_id?: number;
 
   constructor(values: User) {
     Object.assign(this, values);
