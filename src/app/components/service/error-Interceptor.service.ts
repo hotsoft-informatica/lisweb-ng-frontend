2906e247bffca45ac774bcd40eb35adc11528d32
RogerDelNobre <rogerdelnobre@gmail.com>
2023-02-10 09:03:33 -0300

    Ajusta dependencia merge

--- src/app/components/service/error-Interceptor.service.ts
+++ src/app/components/service/error-Interceptor.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { MatLegacyDialogState as MatDialogState } from '@angular/material/legacy-dialog';
+import { MatDialogState } from '@angular/material/dialog';
 import { MatDialog } from '@angular/material/dialog'
 
 export const maxRetries = 2;
