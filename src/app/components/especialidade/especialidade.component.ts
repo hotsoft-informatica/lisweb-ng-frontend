76b219526c7b3bb66501e42b2a6e97a48d958dc1
RogerDelNobre <rogerdelnobre@gmail.com>
2023-03-14 11:10:37 -0300

    Cria componente de especialidade

---
+++ src/app/components/especialidade/especialidade.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-especialidade',
+  templateUrl: './especialidade.component.html',
+  styleUrls: ['./especialidade.component.css']
+})
+export class EspecialidadeComponent {
+
+}
