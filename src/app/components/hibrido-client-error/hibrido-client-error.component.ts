7e507630e04bed0f3c97122822158dc3e1f78cc9
RogerDelNobre <rogerdelnobre@gmail.com>
2023-03-22 16:37:03 -0300

    Implementa componente h_c_error

--- src/app/components/hibrido-client-error/hibrido-client-error.component.ts
+++ src/app/components/hibrido-client-error/hibrido-client-error.component.ts
@@ -1,10 +1,103 @@
-import { Component } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewChild,
+  TemplateRef,
+  Renderer2,
+  ElementRef,
+  Input
+} from '@angular/core';
+import { HibridoClientError } from '../model/hibrido-client-error.model';
+import { HibridoClientErrorService } from '../service/hibrido-client-error.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { merge } from 'rxjs';
+import { Query } from '../model/query.model';
+import { Subject, timer } from 'rxjs';
+import { tap, debounceTime } from 'rxjs/operators';
 @Component({
   selector: 'app-hibrido-client-error',
-  templateUrl: './hibrido-client-error.component.html',
-  styleUrls: ['./hibrido-client-error.component.css']
+  templateUrl: './hibrido-client-error.component.html'
 })
-export class HibridoClientErrorComponent {
+
+export class HibridoClientErrorComponent implements OnInit, AfterViewInit {
+  datasource = new MatTableDataSource<any>([]);
+  records: any[] = [];
+  record!: any;
+  oldRecord: any;
+  currentRecord: any;
+  deletedRecords: any[] = [];
+  query: Query[] = [];
+  id!: number;
+  totalCount!: number;
+
+  @ViewChild('deleteDialog') deleteDialog: TemplateRef<any> | any;
+  @ViewChild(MatSort) sort: MatSort | any;
+  @ViewChild(MatPaginator) paginator: MatPaginator | any;
+
+  queries: Query[] = [];
+
+  onEdit = false;
+  onCreate = false;
+
+  displayedColumns = [
+    'version_id',
+    'http_action',
+    'tabela',
+    'exception_class',
+    'mensagem',
+    'data_hora',
+    'action'
+  ];
+
+  constructor(
+    public dialog: MatDialog,
+    private renderer: Renderer2,
+    private router: Router,
+    private route: ActivatedRoute,
+    private recordService: HibridoClientErrorService
+  ) {
+    this.currentRecord = new HibridoClientError({});
+    this.record ||= new HibridoClientError({});
+  }
+
+  ngOnInit(): void {
+    this.recordService.count().subscribe((totalCount) => {
+      this.totalCount = totalCount;
+    });
+  }
+
+  ngAfterViewInit() {
+    this.loadPage();
+    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0)); // reseta o paginador depois de ordenar
+
+    merge(this.sort.sortChange, this.paginator.page) // Na ordenação ou paginação, carrega uma nova página
+      .pipe(tap(() => this.loadPage()))
+      .subscribe();
+  }
+
+  loadPage() {
+    this.recordService
+      .find(this.sort.active,
+        this.sort.direction,
+        this.paginator.pageIndex,
+        this.paginator.pageSize, this.query
+      ).subscribe((records: any[]) => {
+        this.records = records;
+        this.datasource.data = [...this.records];
+      });
+  }
+
+  search(key: string, value: string, isNumeric: boolean = false): void {
+    const query = new Query({ key, value, isNumeric });
+    this.query = this.query.filter((q) => q.key !== key);
+    this.query.push(query);
+    this.paginator.pageIndex = 0;
+    this.loadPage();
+  }
 
 }
