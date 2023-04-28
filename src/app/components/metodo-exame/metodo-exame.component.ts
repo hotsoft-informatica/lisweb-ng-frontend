835f4d1c32bee849b12fd31f055e9b5d3e0347e1
RogerDelNobre <rogerdelnobre@gmail.com>
2023-04-28 14:02:41 -0300

    Refatora componente metodo exame

--- src/app/components/metodo-exame/metodo-exame.component.ts
+++ src/app/components/metodo-exame/metodo-exame.component.ts
@@ -5,24 +5,29 @@ import { MetodoExameService } from 'src/app/components/service/metodo-exame.serv
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialogModule } from '@angular/material/dialog';
 
-export interface DialogData{
+export interface DialogData {
   metodoExame: MetodoExame;
 }
 @Component({
-    selector: 'app-metodo-exame',
-    templateUrl: './metodo-exame.component.html',
-    standalone: true,
-    imports: [FormsModule, MatFormFieldModule, MatInputModule]
+  selector: 'app-metodo-exame',
+  templateUrl: './metodo-exame.component.html',
+  standalone: true,
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatDialogModule]
 })
 
 export class MetodoExameComponent implements OnInit {
 
-@Input('metodoExame') metodoExame: MetodoExame;
+  @Input('metodoExame') metodoExame: MetodoExame;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData,
-              private metodoExameService: MetodoExameService,
-              ) {
+    private metodoExameService: MetodoExameService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {
     this.metodoExame = this.data.metodoExame;
     this.metodoExame ||= new MetodoExame({});
   }
@@ -33,5 +38,9 @@ export class MetodoExameComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  cancel(): void {
+    this.router.navigate(['/metodos-exames/read']);
   }
 }
