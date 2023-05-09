afa93171b0e92f4d2d7779e5254b127f68f5ce98
RogerDelNobre <rogerdelnobre@gmail.com>
2023-05-09 16:38:10 -0300

    Ticket_id: #112895 - Cria componente classe medico

---
+++ src/app/components/medico/medico.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-medico',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './medico.component.html'
+})
+export class MedicoComponent {
+
+}
