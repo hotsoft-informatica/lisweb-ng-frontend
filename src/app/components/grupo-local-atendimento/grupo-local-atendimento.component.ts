50f8da700bff2208b9646057a1504b065dff687d
Roger <rogerdelnobre@gmail.com>
2023-05-31 16:41:18 -0300

    Ticket_id: #113517 Cria componente grupo_local_atendimento

---
+++ src/app/components/grupo-local-atendimento/grupo-local-atendimento.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grupo-local-atendimento',
+  templateUrl: './grupo-local-atendimento.component.html',
+  styleUrls: ['./grupo-local-atendimento.component.css']
+})
+export class GrupoLocalAtendimentoComponent {
+
+}
