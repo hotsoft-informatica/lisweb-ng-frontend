b5afacdcbfe11ded7476dd699ecafd2854680b4d
Roger <rogerdelnobre@gmail.com>
2023-06-01 13:53:37 -0300

    Ticket_id: #113517 Implementa component de grupo-local-atendimento

--- src/app/components/grupo-local-atendimento/grupo-local-atendimento.component.ts
+++ src/app/components/grupo-local-atendimento/grupo-local-atendimento.component.ts
@@ -1,10 +1,175 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewChild,
+  TemplateRef,
+  ElementRef,
+  Input
+} from '@angular/core';
+import { GrupoLocalAtendimento } from '../model/grupo-local-atendimento.model';
+import { GrupoLocalAtendimentoService } from '../service/grupo-local-atendimento.service';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { merge } from 'rxjs';
+import { Query } from '../model/query.model';
+import { Subject, timer } from 'rxjs';
+import { tap, debounceTime } from 'rxjs/operators';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { NgIf, NgFor } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-grupo-local-atendimento',
   templateUrl: './grupo-local-atendimento.component.html',
-  styleUrls: ['./grupo-local-atendimento.component.css']
+  standalone: true,
+  imports: [
+    CommonModule, MatIconModule, NgIf, MatFormFieldModule, MatInputModule, FormsModule,
+    MatAutocompleteModule, NgFor, MatOptionModule, MatSelectModule, MatTabsModule,
+    MatButtonModule, MatTableModule, MatSortModule, MatDialogModule, MatPaginatorModule,
+    MatDatepickerModule
+  ]
 })
 export class GrupoLocalAtendimentoComponent {
+  gruposLocaisAtendimento: GrupoLocalAtendimento[] =[];
+  datasource = new MatTableDataSource<any>([]);
+  records: any[] = [];
+  record!: any;
+  oldRecord: any;
+  currentRecord: any;
+  deletedRecords: any[] = [];
+  query: Query[] = [];
+  id!: number;
+  totalCount!: number;
+
+  @ViewChild('deleteDialog') deleteDialog: TemplateRef<any> | any;
+  @ViewChild(MatSort) sort: MatSort | any;
+  @ViewChild(MatPaginator) paginator: MatPaginator | any;
+
+  queries: Query[] = [];
+  subjectEspecialidade: Subject<any> = new Subject();
+  subjectOperadoraTelefonia: Subject<any> = new Subject();
+
+  onEdit = false;
+  onCreate = false;
+
+  displayedColumns = [
+    'nome',
+    'action'
+  ];
+
+  constructor(
+    public dialog: MatDialog,
+    private recordService: GrupoLocalAtendimentoService
+  ) {
+    this.currentRecord = new GrupoLocalAtendimento({});
+    this.record ||= new GrupoLocalAtendimento({});
+  }
+
+  ngOnInit(): void {
+    this.recordService.count().subscribe((totalCount) => {
+      this.totalCount = totalCount;
+    });
+
+    const query = new Query({ key: '', value: '', isNumeric: false });
+  }
+
+  ngAfterViewInit() {
+    this.loadPage();
+    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0)); // reseta o paginador depois de ordenar
+
+    merge(this.sort.sortChange, this.paginator.page) // Na ordenação ou paginação, carrega uma nova página
+      .pipe(tap(() => this.loadPage()))
+      .subscribe();
+  }
+
+  loadPage() {
+    this.recordService
+      .find(this.sort.active,
+        this.sort.direction,
+        this.paginator.pageIndex,
+        this.paginator.pageSize, this.query
+      ).subscribe((records: any[]) => {
+        this.records = records;
+        this.datasource.data = [...this.records];
+      });
+  }
+
+  new(): void {
+    this.onCreate = true;
+  }
+
+  addGridData(): void {
+    this.onCreate = false;
+    this.onEdit = false;
+    this.recordService.create(this.currentRecord).subscribe((record) => {
+      // this.records.unshift(record);
+      // this.datasource.data = [...this.records];
+      this.recordService.showMessage('Grupo local de atendimento cadastrado com sucesso!');
+      this.loadPage();
+    });
+
+    this.currentRecord = new GrupoLocalAtendimento({});
+  }
+
+  updateGridData(): void {
+    this.onCreate = false;
+    this.onEdit = false;
+    this.recordService.update(this.currentRecord).subscribe((recurso) => {
+      this.recordService.showMessage('Grupo local de atendimento atualizado com sucesso!');
+      this.loadPage();
+    });
+    this.currentRecord = new GrupoLocalAtendimento({});
+  }
+
+  atualizar(row: GrupoLocalAtendimento): void {
+    console.warn('Passou no atualizar!!!!!!!')
+    console.table(row);
+    this.currentRecord = row;
+    this.onCreate = false;
+    this.onEdit = true;
+  }
+
+  cancelar(): void {
+    this.onCreate = false;
+    this.onEdit = false;
+    Object.assign(this.currentRecord, this.oldRecord);
+    this.currentRecord = new GrupoLocalAtendimento({});
+  }
+
+  deleteGridData(id: number): void {
+    const dialogRef = this.dialog.open(this.deleteDialog);
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.recordService.delete(id)
+          .subscribe((record) => {
+            this.recordService.showMessage('Grupo local de atendimento apagado com sucesso!');
+
+            // Carrega os dados do backend e faz refresh do datasource
+            this.loadPage();
+            this.datasource.data = [...this.records];
+          });
+      }
+    });
+  }
+
+  search(key: string, value: string, isNumeric: boolean = false): void {
+    const query = new Query({ key, value, isNumeric });
+    this.query = this.query.filter((q) => q.key !== key);
+    this.query.push(query);
+    this.paginator.pageIndex = 0;
+    this.loadPage();
+  }
 
 }
