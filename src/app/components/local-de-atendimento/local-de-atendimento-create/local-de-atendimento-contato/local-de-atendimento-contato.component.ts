8b3645f0ef59b8a1f079ff9d06d531a38d95e60c
Roger <rogerdelnobre@gmail.com>
2023-06-05 09:46:13 -0300

    Ticket_id: #113516 Cria componente de local-de-atendimento-contato

---
+++ src/app/components/local-de-atendimento/local-de-atendimento-create/local-de-atendimento-contato/local-de-atendimento-contato.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-local-de-atendimento-contato',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './local-de-atendimento-contato.component.html',
+  styleUrls: ['./local-de-atendimento-contato.component.css']
+})
+export class LocalDeAtendimentoContatoComponent {
+
+}
