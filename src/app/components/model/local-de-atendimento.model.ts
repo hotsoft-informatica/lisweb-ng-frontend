b7bfaa427c3d2c053afcd284fcbdbee98018f853
Roger <rogerdelnobre@gmail.com>
2023-06-05 15:04:35 -0300

    Ticket_id: #113516 Cria relacionamento empresa e local-de-atendimento model

--- src/app/components/model/local-de-atendimento.model.ts
+++ src/app/components/model/local-de-atendimento.model.ts
@@ -1,9 +1,7 @@
-import { Empresa } from '../model/empresa.model';
 import { Base } from './base.model'
+import { Empresa } from '../model/empresa.model';
 
 export class LocalDeAtendimento extends Base{
-  public empresa?: Empresa;
-
   public nome?: string;
   public cnes?: string;
   public grupo_local_id?: number;
@@ -12,19 +10,15 @@ export class LocalDeAtendimento extends Base{
   public grupo_historico?: number;
   public tipo_coleta?: string;
   public coleta_externa?: string;
-
   public empresa_id?: number;
-
   public utiliza_coleta?: string;
   public usa_biometria?: string;
   public painel_monitoramento?: string;
   public utiliza_crm?: string;
   public emitir_etiqueta_apoio_recepcao?: string;
-
   public cod_conselho?: string;
   public sigla_conselho?: string;
   public uf_conselho?: string;
-
   public trabalha_24_horas?: string;
   public trabalha_sabado?: string;
   public trabalha_domingo?: string;
@@ -34,7 +28,6 @@ export class LocalDeAtendimento extends Base{
   public horario_sabado_fim?: string;
   public horario_domingo_inicio?: string;
   public horario_domingo_fim?: string;
-
   public utiliza_normal?: string;
   public utiliza_pouco_urgente?: string;
   public utiliza_urgente?: string;
@@ -45,6 +38,7 @@ export class LocalDeAtendimento extends Base{
   public tempo_urgente?: string;
   public tempo_muito_urgente?: string;
   public tempo_emergencia?: string;
+  public empresa?: Empresa;
 
   constructor(values: LocalDeAtendimento) {
     super(values);
