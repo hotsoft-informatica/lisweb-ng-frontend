7bbc2a74909f8830bc6193092b6a02cf4f267345
Roger <rogerdelnobre@gmail.com>
2023-06-06 16:29:55 -0300

    Ticket_id: #113516 Implementa componente de urgencia em local-de-atendimento

--- src/app/components/local-de-atendimento/local-de-atendimento-create/local-de-atendimento-urgencia/local-de-atendimento-urgencia.component.ts
+++ src/app/components/local-de-atendimento/local-de-atendimento-create/local-de-atendimento-urgencia/local-de-atendimento-urgencia.component.ts
@@ -4,12 +4,16 @@ import { LocalDeAtendimento } from 'src/app/components/model/local-de-atendiment
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-local-de-atendimento-urgencia',
   standalone: true,
   templateUrl: './local-de-atendimento-urgencia.component.html',
-  imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule]
+  imports: [CommonModule, FormsModule, MatFormFieldModule,
+    MatInputModule, MatOptionModule, MatSelectModule
+  ]
 })
 export class LocalDeAtendimentoUrgenciaComponent implements OnInit {
   @Input('localdeatendimento') localdeatendimento: LocalDeAtendimento;
