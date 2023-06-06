31944161900a25dcb79b7657d46bf7f65b05aeb6
Roger <rogerdelnobre@gmail.com>
2023-06-06 16:27:13 -0300

    Ticket_id: #113516 Implementa componente de contato em local-de-atendimento

--- src/app/components/local-de-atendimento/local-de-atendimento-create/local-de-atendimento-contato/local-de-atendimento-contato.component.ts
+++ src/app/components/local-de-atendimento/local-de-atendimento-create/local-de-atendimento-contato/local-de-atendimento-contato.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Empresa } from 'src/app/components/model/empresa.model';
+import { LocalDeAtendimento } from 'src/app/components/model/local-de-atendimento.model';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-local-de-atendimento-contato',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './local-de-atendimento-contato.component.html',
-  styleUrls: ['./local-de-atendimento-contato.component.css']
+  imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule]
 })
-export class LocalDeAtendimentoContatoComponent {
+export class LocalDeAtendimentoContatoComponent implements OnInit {
+  @Input('localdeatendimento') localdeatendimento: LocalDeAtendimento;
+  @Input('empresa') empresa: Empresa;
 
+  constructor() {
+    this.localdeatendimento = new LocalDeAtendimento({});
+    this.empresa ||= new Empresa({});
+    console.table(this.empresa);
+    this.localdeatendimento.empresa = this.empresa;
+  }
+  ngOnInit(): void {
+    this.empresa ||= new Empresa({});
+  }
 }
