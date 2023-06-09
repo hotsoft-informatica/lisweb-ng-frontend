210e32aa390f93e2675ec817a6bc1268df0301b8
Roger <rogerdelnobre@gmail.com>
2023-06-09 16:24:34 -0300

    Ticket_id: #113516  Ajusta Local Atendimento empresa componente

--- src/app/components/local-de-atendimento/local-de-atendimento-create/local-de-atendimento-empresa/local-de-atendimento-empresa.component.ts
+++ src/app/components/local-de-atendimento/local-de-atendimento-create/local-de-atendimento-empresa/local-de-atendimento-empresa.component.ts
@@ -13,7 +13,6 @@ import { FormsModule } from '@angular/forms';
     imports: [FormsModule, MatFormFieldModule, MatInputModule]
 })
 export class LocalDeAtendimentoEmpresaComponent implements OnInit {
-
   @Input('localdeatendimento') localdeatendimento: LocalDeAtendimento;
   @Input('empresa') empresa: Empresa;
 
