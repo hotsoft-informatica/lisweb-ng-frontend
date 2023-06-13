721fe7a168b630e9a5ca52b159b94e458fba6a40
Roger <rogerdelnobre@gmail.com>
2023-06-13 15:45:54 -0300

    Ticket_id: #113742  Implementa mat-slide-toggle componente em horario_funcionamento

--- src/app/components/local-de-atendimento/local-de-atendimento-create/local-de-atendimento-horario-funcionamento/local-de-atendimento-horario-funcionamento.component.ts
+++ src/app/components/local-de-atendimento/local-de-atendimento-create/local-de-atendimento-horario-funcionamento/local-de-atendimento-horario-funcionamento.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnChanges, Input } from '@angular/core';
 import { LocalDeAtendimento } from 'src/app/components/model/local-de-atendimento.model';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSlideToggleChange, MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-local-de-atendimento-horario-funcionamento',
@@ -11,17 +11,22 @@ import { FormsModule } from '@angular/forms';
     standalone: true,
     imports: [FormsModule, MatFormFieldModule, MatInputModule, MatSlideToggleModule]
 })
-export class LocalDeAtendimentoHorarioFuncionamentoComponent implements OnInit {
-  integral = false;
-
+export class LocalDeAtendimentoHorarioFuncionamentoComponent implements OnChanges {
   @Input('localdeatendimento') localdeatendimento: LocalDeAtendimento;
-
+  trabalha_24_horas = false;
 
   constructor() {
-    this.localdeatendimento = new LocalDeAtendimento({});
+    this.localdeatendimento ||= new LocalDeAtendimento({});
   }
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    if (this.localdeatendimento) {
+      this.trabalha_24_horas = this.localdeatendimento.trabalha_24_horas != 'S' ? false : true;
+    }
   }
 
+  toggleChanges(e: MatSlideToggleChange){
+    this.trabalha_24_horas = e.source.checked
+    this.localdeatendimento.trabalha_24_horas = this.trabalha_24_horas ? 'S' : 'N';
+  }
 }
