398d61a489417aa7277d9f9a732f2bda999ee96b
Roger <rogerdelnobre@gmail.com>
2023-06-26 10:48:54 -0300

    Ticket_id: #114016 Cria service de convenio

---
+++ src/app/components/service/convenio.service.ts
@@ -0,0 +1,17 @@
+import { BaseService } from './base.service';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, Inject, Injector } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConvenioService extends BaseService {
+  baseUrl = '/convenios';
+
+  constructor(
+    @Inject(Injector) public injector: Injector,
+    public http: HttpClient) {
+    super(injector, http);
+    this.endpoint = 'convenios'
+  }
+}
