fbd3e0a1bdd91548b062bc51c623b4c65c2bb354
Roger <rogerdelnobre@gmail.com>
2023-07-05 15:29:51 -0300

    Ticket_id: #114207 Cria componete aba detalhes de convenio

---
+++ src/app/components/convenio/convenio-detalhe/convenio-detalhe.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-convenio-detalhe',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './convenio-detalhe.component.html',
+  styleUrls: ['./convenio-detalhe.component.css']
+})
+export class ConvenioDetalheComponent {
+
+}
