acf54fdbfa72819bdbf3b975ef1b9dca0e1deaa8
Roger <rogerdelnobre@gmail.com>
2023-07-07 15:07:08 -0300

    Ticket_id: #114207 Implementa input autocomplete custom de relatorio de guia

--- src/app/components/convenio/convenio-detalhe/convenio-detalhe.component.ts
+++ src/app/components/convenio/convenio-detalhe/convenio-detalhe.component.ts
@@ -1,13 +1,75 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, Input } from '@angular/core';
+import { Convenio } from '../../model/convenio.model';
+import { ConvenioService } from '../../service/convenio.service';
+import { Relatorio } from '../../model/relatorio.model';
+import { RelatorioGuiaService } from '../../service/relatorio-guia.service';
+import { Query } from 'src/app/components/model/query.model';
+import { Subject, debounceTime } from 'rxjs';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { NgFor } from '@angular/common';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-convenio-detalhe',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './convenio-detalhe.component.html',
-  styleUrls: ['./convenio-detalhe.component.css']
+  imports: [ FormsModule, MatFormFieldModule, MatInputModule,
+    MatAutocompleteModule, NgFor, MatOptionModule, MatSelectModule
+  ]
 })
-export class ConvenioDetalheComponent {
+export class ConvenioDetalheComponent implements OnInit {
+  convenio: Convenio;
 
+  @Input('relatorios') relatorios: Relatorio[] = [];
+
+  queries: Query[] = [];
+
+  subjectRelatorio: Subject<any> = new Subject();
+  subjectMetodo: Subject<any> = new Subject();
+
+  constructor(
+    private convenioService: ConvenioService,
+    private relatorioGuiaService: RelatorioGuiaService
+  ) {
+    this.convenio ||= new Convenio({});
+  }
+
+  ngOnInit(): void {
+    const query = new Query({ key: '', value: '', isNumeric: false });
+
+    this.subjectRelatorio.pipe(debounceTime(500)).subscribe(() => {
+      this.relatorioGuiaService
+        .find('id', 'asc', 0, 60, this.queries)
+        .subscribe((relatorios: any) => {
+          console.table(this.queries);
+          this.relatorios = relatorios;
+        });
+    });
+    this.subjectRelatorio.next(null);
+  }
+
+  searchRelatorio(): void {
+    const query_string = this.convenio.idrelatorioguia as unknown as string;
+    const query = new Query({
+      key: 'titulo',
+      value: query_string,
+      isNumeric: false,
+    });
+    this.queries = [];
+    this.queries.push(query);
+    this.subjectRelatorio.next(null);
+  }
+
+  displayFnRelatorio(options: Relatorio[]): (id: any) => any {
+    return (id: any) => {
+      const correspondingOption = Array.isArray(options)
+        ? options.find((option) => option.id === id)
+        : null;
+      return correspondingOption ? correspondingOption.titulo : '';
+    };
+  }
 }
