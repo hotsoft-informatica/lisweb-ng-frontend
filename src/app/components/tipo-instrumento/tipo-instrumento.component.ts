061cea47b0a290b1683a370f1feb28a28a060c70
Roger <rogerdelnobre@gmail.com>
2023-07-18 16:20:41 -0300

    Ticket_id: #114643 Cria componente tipo instrumento

---
+++ src/app/components/tipo-instrumento/tipo-instrumento.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tipo-instrumento',
+  standalone: true,
+  templateUrl: './tipo-instrumento.component.html',
+  imports: [CommonModule]
+
+})
+export class TipoInstrumentoComponent {
+
+}
