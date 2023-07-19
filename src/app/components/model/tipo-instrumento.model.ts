711fcf4090fedf772dbee35f213c8704f6b0c11d
Roger <rogerdelnobre@gmail.com>
2023-07-19 09:58:40 -0300

    Ticket_id: #114643 Ajusta model de tipo instrumento

--- src/app/components/model/tipo-instrumento.model.ts
+++ src/app/components/model/tipo-instrumento.model.ts
@@ -1,20 +1,32 @@
 import { Base } from './base.model';
+import { Driver } from './driver.model';
 
 export class TipoInstrumento extends Base{
-  public aprova_exame_bancada?: string;
-  public cod_instrumento?: string;
+  public id?: number;
+  public laboratorio_id?: number;
   public descricao?: string;
+  public padrao_cod_barras?: number;
   public diretorio_transf_dados?: string;
+  public cod_instrumento?: string;
+  public aprova_exame_bancada?: string;
   public habilita_triagem_rapida?: string;
   public interfaceamento?: string;
   public liberacao_tecnica?: string;
   public realiza_query?: string;
   public sobrescreve_resultado?: string;
-
-  public driver_id?: number;
-  public modalidade?: number;
-  public padrao_cod_barras?: number;
+  public original_id?: number;
+  public created_at?: Date = new Date();
+  public updated_at?: Date = new Date();
   public relatorio_id?: number;
+  public driver_id?: number;
+  public version_id?: number;
+  public modalidade?: string;
+  public laboratory_domain_id?: number;
+  public deleted?: boolean;
+  public criado_em?: Date = new Date();
+  public changed_by_lab_id?: number;
+  public uuid?: string;
+  public drivers?: Driver;
 
   constructor(values: TipoInstrumento) {
     super(values);
