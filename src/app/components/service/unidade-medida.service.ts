791e5709af166b0ec6d3041e7e8d70f95269aa1f
Roger <rogerdelnobre@gmail.com>
2023-08-03 16:42:25 -0300

    Cria service de unidade medida

---
+++ src/app/components/service/unidade-medida.service.ts
@@ -0,0 +1,16 @@
+import { BaseService } from './base.service';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, Inject, Injector } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UnidadeMedidaService extends BaseService {
+  constructor(
+    @Inject(Injector) public injector: Injector,
+    public http: HttpClient,
+  ) {
+    super(injector, http);
+    this.endpoint = 'unidades_medida'
+  }
+}
