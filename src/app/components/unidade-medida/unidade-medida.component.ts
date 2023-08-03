1183dcc1343a1e20b588cb26dfce0d443afae4d2
Roger <rogerdelnobre@gmail.com>
2023-08-03 16:19:31 -0300

    Cria componente de unidade medida

---
+++ src/app/components/unidade-medida/unidade-medida.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-unidade-medida',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './unidade-medida.component.html',
+  styleUrls: ['./unidade-medida.component.css']
+})
+export class UnidadeMedidaComponent {
+
+}
