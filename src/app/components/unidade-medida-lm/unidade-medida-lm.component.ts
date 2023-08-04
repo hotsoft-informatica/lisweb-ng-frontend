a4fe049b30dce0289dccfe4485551f0ece10869e
Roger <rogerdelnobre@gmail.com>
2023-08-04 11:12:24 -0300

    Implementa componente para unidade_medida_lm

---
+++ src/app/components/unidade-medida-lm/unidade-medida-lm.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-unidade-medida-lm',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './unidade-medida-lm.component.html',
+  styleUrls: ['./unidade-medida-lm.component.css']
+})
+export class UnidadeMedidaLmComponent {
+
+}
