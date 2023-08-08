1db494edfdca5087d218ac9226179e674db8ed48
Roger <rogerdelnobre@gmail.com>
2023-08-08 11:22:09 -0300

    Ticket_id: #115069 Cria componente de fornecedor sincronizacao

---
+++ src/app/components/fornecedor-sincronizacao/fornecedor-sincronizacao.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-fornecedor-sincronizacao',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './fornecedor-sincronizacao.component.html',
+  styleUrls: ['./fornecedor-sincronizacao.component.css']
+})
+export class FornecedorSincronizacaoComponent {
+
+}
