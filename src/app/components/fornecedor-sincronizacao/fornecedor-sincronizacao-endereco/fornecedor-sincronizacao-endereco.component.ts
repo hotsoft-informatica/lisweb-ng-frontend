6fec71f6b44549bafb00ce7ab0a74a3d9459cdf9
Roger <rogerdelnobre@gmail.com>
2023-08-10 16:34:38 -0300

    Ticket_id: #115069 Implementa Aba para criacao de endereco empresa vinculada ao fornecedor

---
+++ src/app/components/fornecedor-sincronizacao/fornecedor-sincronizacao-endereco/fornecedor-sincronizacao-endereco.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-fornecedor-sincronizacao-endereco',
+  templateUrl: './fornecedor-sincronizacao-endereco.component.html',
+  standalone: true,
+  imports: [CommonModule],
+})
+export class FornecedorSincronizacaoEnderecoComponent {
+
+}
