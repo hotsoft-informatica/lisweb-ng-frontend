4dd90d7107f98d6aaff555c39567d33386ee1528
Roger <rogerdelnobre@gmail.com>
2023-08-11 16:15:14 -0300

    Ticket_id: #115069 Cria componente contato para fornecedor e empresa

---
+++ src/app/components/fornecedor-sincronizacao/fornecedor-sincronizacao-contato/fornecedor-sincronizacao-contato.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-fornecedor-sincronizacao-contato',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './fornecedor-sincronizacao-contato.component.html',
+  styleUrls: ['./fornecedor-sincronizacao-contato.component.css']
+})
+export class FornecedorSincronizacaoContatoComponent {
+
+}
