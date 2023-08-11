59089d03941e79c94f0f2364f32ba51f9a4567e4
Roger <rogerdelnobre@gmail.com>
2023-08-11 17:05:40 -0300

    Ticket_id: #115069 Implementa componente Aba contato para fornecedor e empresa

--- src/app/components/fornecedor-sincronizacao/fornecedor-sincronizacao-contato/fornecedor-sincronizacao-contato.component.ts
+++ src/app/components/fornecedor-sincronizacao/fornecedor-sincronizacao-contato/fornecedor-sincronizacao-contato.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Empresa } from 'src/app/components/model/empresa.model';
+import { FornecedorSincronizacao } from '../../model/fornecedor-sincronizacao.model';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-fornecedor-sincronizacao-contato',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './fornecedor-sincronizacao-contato.component.html',
-  styleUrls: ['./fornecedor-sincronizacao-contato.component.css']
+  standalone: true,
+  imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule]
 })
-export class FornecedorSincronizacaoContatoComponent {
+export class FornecedorSincronizacaoContatoComponent implements OnInit {
+  @Input('fornecedorSincronizacao') fornecedorSincronizacao: FornecedorSincronizacao;
+  @Input('empresa') empresa: Empresa;
+
+  constructor() {
+    this.fornecedorSincronizacao = new FornecedorSincronizacao({});
+    this.empresa ||= new Empresa({});
+    // console.table(this.empresa);
+    this.fornecedorSincronizacao.empresa = this.empresa;
+  }
+  ngOnInit(): void {
+    this.empresa ||= new Empresa({});
+  }
 
 }
