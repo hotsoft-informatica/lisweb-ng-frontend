c7a4e8acb21eae42318b6aa7616dbccb03758d0c
Roger <rogerdelnobre@gmail.com>
2023-08-16 11:29:42 -0300

    Ticket_id: #115250 Cria componente de material-consumo

---
+++ src/app/components/material-consumo/material-consumo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-material-consumo',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './material-consumo.component.html',
+  styleUrls: ['./material-consumo.component.css']
+})
+export class MaterialConsumoComponent {
+
+}
