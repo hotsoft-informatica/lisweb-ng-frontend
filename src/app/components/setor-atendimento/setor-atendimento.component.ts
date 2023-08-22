d3dd4cbfb56cc6925365dbaf82dbc06a83956370
Roger <rogerdelnobre@gmail.com>
2023-08-22 14:48:57 -0300

    Ticket_id: #115404 Cria componente para Setor Atendimento

---
+++ src/app/components/setor-atendimento/setor-atendimento.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-setor-atendimento',
+  templateUrl: './setor-atendimento.component.html',
+  standalone: true,
+  imports: [CommonModule]
+
+})
+export class SetorAtendimentoComponent {
+
+}
