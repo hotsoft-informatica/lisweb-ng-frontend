f8b0a823cf1b097ea9e38e4aaca0e340e6ab6530
Roger <rogerdelnobre@gmail.com>
2023-09-06 15:14:58 -0300

    Ticket_id: #115727 Cria componente de prioridade coleta

---
+++ src/app/components/prioridade-coleta/prioridade-coleta.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-prioridade-coleta',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './prioridade-coleta.component.html',
+  styleUrls: ['./prioridade-coleta.component.css']
+})
+export class PrioridadeColetaComponent {
+
+}
